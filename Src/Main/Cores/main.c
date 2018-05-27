f33db64aa3c4367486751974409f1d8ea9210330
Alireza Rashti <arashti2016@fau.edu>
2018-05-27 18:45:14 -0400

    Sketching the general form of main.c

--- Src/Main/Cores/main.c
+++ Src/Main/Cores/main.c
@@ -1,6 +1,21 @@
+/*
+// Alireza Rashti
+// May 2018
+*/
+
 #include "main.h"
 
-void main()
+int main(int argn, char **argv, char **argv2)
 {
-    printf("hi\n");
+  read_input_file(argv[argn-1]);
+  
+  add_projets();//add all of the desired projects to the project data base
+  
+  proj1 = get_project("BNS_Initial_Data");
+  project_execute(proj1);
+  
+  project_cleanup(proj1);
+  data_base_cleanup();
+  
+  return EXIT_SUCCESS;
 }
