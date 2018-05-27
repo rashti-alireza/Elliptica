ac59719505e88526657e39c2036aca04c93d2d22
Alireza Rashti <arashti2016@fau.edu>
2018-05-27 18:50:22 -0400

    Initiating core_lib.h

---
+++ Src/Main/Cores/core_lib.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <float.h>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+#include "typdef_data.h"
+#include "typdef_functions.h"
