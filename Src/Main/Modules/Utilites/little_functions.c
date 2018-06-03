00de301daced06a8e20cd26bd06412e1b1f54f40
Alireza Rashti <arashti2016@fau.edu>
2018-06-03 17:52:32 -0400

    Add function to show file and line, used for testing at the middle of a function

---
+++ Src/Main/Modules/Utilites/little_functions.c
@@ -0,0 +1,14 @@
+/*
+// Alireza Rashti
+// June 2018
+*/
+
+#include "little_functions.h"
+
+/* helps you to find where litte tests start*/
+void test_start(char *file,int line)
+{
+  printf("Test starts at\n"
+    "File:%s\nLine:%d\n",file,line);
+}
+
