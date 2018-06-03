2bbfb7204f75e3ee1b41a5dcbb6f1051ad42f338
Alireza Rashti <arashti2016@fau.edu>
2018-06-02 23:28:49 -0400

    Adding global variables for output directories

--- Src/Main/Cores/global_variables.c
+++ Src/Main/Cores/global_variables.c
@@ -6,9 +6,57 @@
 #include "global_variables.h"
 
 /* initiate global variables */
-int global_variables_init(void)
+int global_variables_init(char *path)
 {
   global_parameter = 0;
+  make_global_path(path);
+  find_inputfile_name(path);
   
   return EXIT_SUCCESS;
 }
+
+/* finding inputfile name */
+static void find_inputfile_name(char *path)
+{
+  char *first,*last,*p;
+  char name[1000] = {'\0'};
+  
+  first = strrchr(path,'\\');
+  pointerEr(first);
+  
+  last = strrchr(path,'.');
+  pointerEr(last);
+  
+  if (last <= first)
+  {
+    fprintf(stderr,ERROR_MASSAGE"The name of input file must be like:"
+    " \"input.in\"\n");
+    printf(ERROR_MASSAGE_EXIT);
+    abort();
+    
+  }
+  
+  for (p = first; *p != '.'; p++)
+    name[p-first] = first[p-first];
+  
+  global_inputfile_name = strdup(name);
+  
+}
+
+/* making global_path */
+static void make_global_path(char *path)
+{
+  char *first,*last,*p;
+  char dir[1000] = {'\0'};
+  
+  first = strchr(path,'\\');
+  pointerEr(first);
+  last = strrchr(path,'\\');
+  pointerEr(last);
+  
+  for (p = first; p < last; p++)
+    dir[p-first] = first[p-first];
+  
+  global_path = strdup(dir);
+}
+
--- Src/Main/Cores/global_variables.h
+++ Src/Main/Cores/global_variables.h
@@ -1,4 +1,11 @@
 #include "core_lib.h"
+#include "error_handling_lib.h"
 
 /* global variables */
-Parameter_T **global_parameter;
+Parameter_T **global_parameter;// parameters
+char *global_path;// path of a directory where input file is
+char *global_inputfile_name; //name of inputfile
+
+
+static void make_global_path(char *path);
+static void find_inputfile_name(char *path);
