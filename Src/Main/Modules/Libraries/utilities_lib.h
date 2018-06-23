954eb0be470bfc54c5f9de8ddd1e67289f43e302
Alireza Rashti <arashti2016@fau.edu>
2018-06-23 12:39:11 -0400

    Renaming and adding extra function for utilities

---
+++ Src/Main/Modules/Libraries/utilities_lib.h
@@ -0,0 +1,12 @@
+void test_start(char *file,int line);
+int countf(void *p);
+
+#define TEST_START test_start(__FILE__,__LINE__);
+
+
+void IJK(int l, int *n, int *i, int *j, int *k);
+int L(int *n, int i, int j, int k);
+int I(int l, int *n);
+int J(int l, int *n);
+int K(int l, int *n);
+
