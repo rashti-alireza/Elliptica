c86386b4cc74a44aa33736fcc1bb1060ea170abc
Alireza Rashti <arashti2016@fau.edu>
2018-06-23 12:35:25 -0400

    Making a new file for all of used macros; so I deleted macros which were in this file

--- Src/Main/Modules/Libraries/error_handling_lib.h
+++ Src/Main/Modules/Libraries/error_handling_lib.h
@@ -3,14 +3,4 @@ void bad_input_error(char *file, int line);
 void null_path_error(void *path,char *file, int line);
 void abort_error(char *massage,char *file, int line);
 void abort_error_string(char *massage,char *detail,char *file, int line);
-
-
-#define pointerEr(x)   checkup_pointer_error(x,__FILE__,__LINE__)
-#define bad_inputEr()  bad_input_error(__FILE__,__LINE__)
-#define null_pathEr(x) null_path_error(x,__FILE__,__LINE__)
-#define abortEr(x)     abort_error(x,__FILE__,__LINE__)
-#define abortEr_s(x,y)     abort_error_string(x,y,__FILE__,__LINE__)
-#define ERROR_MASSAGE       "\n\t!!!!!!ERROR and ABORT!!!!!!!!!\n"
-#define ERROR_MASSAGE_ABORT   "\t!!!!!!ABORT DUO TO ERROR!!!!!!\n"
-
-
+void check_parameter(Flag_T flg,char *file, int line);
