d00d039c9331b9734a01cc778842acde9fed7128
Alireza Rashti <arashti2016@fau.edu>
2018-06-26 20:49:03 -0400

    Add macros for comparison considering round off error

--- Src/Main/Cores/core_lib.h
+++ Src/Main/Cores/core_lib.h
@@ -13,6 +13,13 @@
 #include "macros_lib.h"
 #include "text_tools_lib.h"
 
+#define ROUND_OFF_ERR 1E-10
+#define LSS(x,y) (x < y-ROUND_OFF_ERR)
+#define GRT(x,y) (x > y+ROUND_OFF_ERR)
+#define EQL(x,y) (x < y+ROUND_OFF_ERR && x > y-ROUND_OFF_ERR)
+#define LSSEQL(x,y) (LSS(x,y) || EQL(x,y))
+#define GRTEQL(x,y) (GRT(x,y) || EQL(x,y))
+
 int    get_parameter_value_I(char *const par_name,Flag_T *flg);
 double get_parameter_value_D(char *const par_name,Flag_T *flg);
 char  *get_parameter_value_S(char *const par_name,Flag_T *flg);
