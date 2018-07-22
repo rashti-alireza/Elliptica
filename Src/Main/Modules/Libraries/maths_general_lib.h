5ab334608f756d68ed3c26159ac0fb798d430afb
Alireza Rashti <arashti2016@fau.edu>
2018-07-22 00:46:49 -0400

    Adding a function to take second order derivative when plausible, using Chebyshev Tn properties.

--- Src/Main/Modules/Libraries/maths_general_lib.h
+++ Src/Main/Modules/Libraries/maths_general_lib.h
@@ -13,4 +13,6 @@ long double rms_l(const unsigned long n, const double *v2, const double *v1);
 double rms(const unsigned n, const double *const v2, const double *const v1);
 double dot(const unsigned n, const double *const v2, const double *const v1);
 double ABS(const double v);
+double Cheb_Tn(const int n, const double x);
 double Cheb_Un(const int n, const double x);
+double d2T_dx2(const int n, const double x);
