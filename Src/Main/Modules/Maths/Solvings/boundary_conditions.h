e35df703b7da6738bfbe4bbe3f6ba7e3d4f326f0
Alireza Rashti <arashti2016@fau.edu>
2018-08-18 18:29:00 -0400

    Initiating general purposes boundary condition functions.

---
+++ Src/Main/Modules/Maths/Solvings/boundary_conditions.h
@@ -0,0 +1,6 @@
+#include "core_lib.h"
+#include "utilities_lib.h"
+#include "error_handling_lib.h"
+#include "memory_managing_lib.h"
+
+int setup_boundary_conditions_patch_wise(Patch_T *const patch);
