7600c403160c11872c6f162b56c1e70a49b8f32a
Alireza Rashti <arashti2016@fau.edu>
2018-08-23 10:58:22 -0400

    Adding a function for initialization of field with initial value for in ital condition.

---
+++ Src/Projects/Poisson/Laplace_Inhom/laplace_inhom_initial_data.c
@@ -0,0 +1,26 @@
+/*
+// Alireza Rashti
+// August 2018
+*/
+
+#include "laplace_inhom_initial_data.h"
+
+/* initial data for field alpha
+// ->return value: EXIT_SUCCESS
+*/
+int Laplace_Inhom_initial_data_alpha(Grid_T *const grid)
+{
+  unsigned p;
+  
+  FOR_ALL_PATCHES(p,grid)
+  {
+    Patch_T *patch = grid->patch[p];
+    double *alpha = patch->pool[Ind("alpha")]->v;
+    unsigned n;
+    
+    FOR_ALL_POINTS(n,patch)
+      alpha[n] = 1;
+  }
+  
+  return EXIT_SUCCESS;
+}
---
+++ Src/Projects/Poisson/Laplace_Inhom/laplace_inhom_initial_data.h
@@ -0,0 +1,3 @@
+#include "core_lib.h"
+
+int Laplace_Inhom_initial_data_alpha(Grid_T *const grid);
