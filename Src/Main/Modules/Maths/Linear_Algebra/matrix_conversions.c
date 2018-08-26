0231eec9004bae9466946040b5b8c4ad603ae7b4
Alireza Rashti <arashti2016@fau.edu>
2018-08-26 15:48:08 -0400

    Adding a function to convert a regular matrix to CCS one.

--- Src/Main/Modules/Maths/Linear_Algebra/matrix_conversions.c
+++ Src/Main/Modules/Maths/Linear_Algebra/matrix_conversions.c
@@ -17,7 +17,11 @@ Matrix_T *cast_matrix_ccs(Matrix_T *const m)
   
   if (m->reg_f)
   {
-    convert_reg2ccs(m,ccs);
+    double DropLimit = 0;
+    if (get_parameter("Ignore_Number_Less_Than"))	
+      DropLimit = GetParameterD("Ignore_Number_Less_Than");
+      
+    convert_reg2ccs(m,ccs,DropLimit);
   }
   else if (m->tri_f)
   {
@@ -37,7 +41,42 @@ Matrix_T *cast_matrix_ccs(Matrix_T *const m)
   return ccs;
 }
 
-/* converting a regular storag format to ccs format */
-static void convert_reg2ccs(const Matrix_T *const reg,Matrix_T *const ccs)
+/* converting a regular storag format to ccs format.
+// note: the matrix will be made in CCS such that be in order in row.
+*/
+static void convert_reg2ccs(const Matrix_T *const reg,Matrix_T *const ccs,const double DropLimit)
 {
+  const long unsigned Nr = reg->row;
+  const long unsigned Nc = reg->col;
+  double **const m = reg->reg->A;
+  long *Ap   = calloc(Nc+1,sizeof(*Ap));
+  long *Ai   = 0;
+  double *Ax = 0;
+  long tNN0 = 0;/* total number of none zero entries */
+  long NN0;/* number of none zero entries in each columnt */
+  long unsigned r,c;/* row and column */
+  
+  for (c = 0; c < Nc; ++c)
+  {
+    NN0 = 0;
+    for (r = 0; r < Nr; ++r)
+    {
+      if (GRT(m[r][c],DropLimit))
+      {
+        Ai = realloc(Ai,(long unsigned)(Ap[c]+NN0+1)*sizeof(*Ai));
+        pointerEr(Ai);
+        Ax = realloc(Ax,(long unsigned)(Ap[c]+NN0+1)*sizeof(*Ax));
+        pointerEr(Ax);
+        Ai[Ap[c]+NN0] = (long)r;
+        Ax[Ap[c]+NN0] = m[r][c];
+        NN0++;
+        tNN0++;
+      }
+    }
+    Ap[c+1] = tNN0;
+  }
+  
+  ccs->ccs->Ap = Ap;
+  ccs->ccs->Ai = Ai;
+  ccs->ccs->Ax = Ax;
 }
--- Src/Main/Modules/Maths/Linear_Algebra/matrix_conversions.h
+++ Src/Main/Modules/Maths/Linear_Algebra/matrix_conversions.h
@@ -4,4 +4,4 @@
 #include "error_handling_lib.h"
 
 Matrix_T *cast_matrix_ccs(Matrix_T *const m);
-static void convert_reg2ccs(const Matrix_T *const reg,Matrix_T *const ccs);
+static void convert_reg2ccs(const Matrix_T *const reg,Matrix_T *const ccs,const double DropLimit);
