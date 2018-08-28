5a7d6fbe1d5507701d100e5fa2ee2c7ddd0ad91f
Alireza Rashti <arashti2016@fau.edu>
2018-08-28 09:24:17 -0400

    Adding a whole seprate file for Parallel patch method and completing the algorithm. Now is time to proof read it and fixing bugs!

--- Src/Main/Modules/Maths/Solvings/solve_eqs.h
+++ Src/Main/Modules/Maths/Solvings/solve_eqs.h
@@ -1,31 +1,13 @@
 #include "core_lib.h"
-#include "utilities_lib.h"
+//#include "utilities_lib.h"
 #include "error_handling_lib.h"
-#include "maths_calculus_lib.h"
-#include "coordinates_lib.h"
-#include "maths_approximation_lib.h"
-#include "memory_managing_lib.h"
-#include "maths_solvers_lib.h"
-
-#define OMP_PARALLEL_PATCH(x) _Pragma ( #x )
-static const double _Dirac_Delta_[2] = {0.0,1.0};/* dirac's delta */
-#define Dirac_Delta(x,y) _Dirac_Delta_[((x)==(y))%2]
+//#include "maths_calculus_lib.h"
+//#include "coordinates_lib.h"
+//#include "maths_approximation_lib.h"
+//#include "memory_managing_lib.h"
+//#include "maths_solvers_lib.h"
 
 typedef int fSolve_T (Grid_T *const grid);
 
 int solve_eqs(Grid_T *const grid);
-Matrix_T *get_j_matrix(const Patch_T *const patch,const char *type);
-void prepare_Js_jacobian_eq(Patch_T *const patch,const char * const *types);
-static int solve_ax_b_ppm(Patch_T *const patch,const unsigned cn);
-static int parallel_patch_method (Grid_T *const grid);
-static int b_bndry_outerB_ppm(Boundary_Condition_T *const bc);
-static int b_bndry_copy_ppm(Boundary_Condition_T *const bc);
-static int b_bndry_interpolate_ppm(Boundary_Condition_T *const bc);
-static int a_bndry_outerB_ppm(Boundary_Condition_T *const bc);
-static int a_bndry_copy_ppm(Boundary_Condition_T *const bc);
-static int a_bndry_interpolate_ppm(Boundary_Condition_T *const bc);
-static int b_in_ax_b_whole_ppm(Patch_T *const patch,const unsigned cn);
-static int b_in_ax_b_bndry_ppm(Patch_T *const patch,const unsigned cn);
-static int a_in_ax_b_whole_ppm(Patch_T *const patch,const unsigned cn);
-static int a_in_ax_b_bndry_ppm(Patch_T *const patch,const unsigned cn);
-static double *normal_vec_curvilinear(Point_T *const point);
+int parallel_patch_method (Grid_T *const grid);
