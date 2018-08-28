ec54f32f3dad0431b1b626c80be2e959fd53d5b0
Alireza Rashti <arashti2016@fau.edu>
2018-08-28 09:21:29 -0400

    Removing unwanted files.

--- Src/Main/Modules/Maths/Solvings/boundary_conditions.h
+++
@@ -1,6 +0,0 @@
-#include "core_lib.h"
-#include "utilities_lib.h"
-#include "error_handling_lib.h"
-#include "memory_managing_lib.h"
-
-int setup_boundary_conditions_patch_wise(Patch_T *const patch);
