7dbc0840d860129409da82fa7704d19b6a9ad1a3
Alireza Rashti <arashti2016@fau.edu>
2018-08-29 16:10:14 -0400

    Adding new functions at Linear_Algebra to the pertinent library.

--- Src/Main/Modules/Libraries/maths_linear_algebra_lib.h
+++ Src/Main/Modules/Libraries/maths_linear_algebra_lib.h
@@ -1,1 +1,5 @@
 Matrix_T *cast_matrix_ccs(Matrix_T *const m);
+void copy_reg2reg(const Matrix_T *const reg1,Matrix_T *const reg2);
+void copy_ccs2ccs(const Matrix_T *const ccs1,Matrix_T *const ccs2);
+void matrix_tests(void);
+
