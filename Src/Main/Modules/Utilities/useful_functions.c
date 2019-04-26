b6d1a5e9ef279594d5173abefffa2838c07c17cf
Alireza Rashti <arashti2016@fau.edu>
2019-04-26 15:07:27 -0400

    Improving the estimation of max error.

--- Src/Main/Modules/Utilities/useful_functions.c
+++ Src/Main/Modules/Utilities/useful_functions.c
@@ -412,7 +412,7 @@ double max_Jacobian_dX_dx(const Patch_T *const patch)
 // 1e-14*max(func)*max(Jacobian)^(order of derivative )*n*n^(2*order of derivative)*10  */
 double spectral_derivative_max_error(const Field_T *const f,const unsigned o)
 {
-  double e = 0;
+  double e = 1e-15;
   double max_f,
          max_j;
   unsigned max_n;
@@ -425,10 +425,12 @@ double spectral_derivative_max_error(const Field_T *const f,const unsigned o)
     max_j = max_Jacobian_dX_dx(f->patch);
     max_n =  n[0] > n[1] ? n[0]  : n[1];
     max_n = max_n > n[2] ? max_n : n[2];
-    e = 1e-14*max_f*max_n*pow(max_n,2*o);/* 1e-14 coming from: (machine precision=1e-15)*(all other unwarranted factors=10) */
+    e = 1e-14*max_n*pow(max_n,2*o);/* 1e-14 coming from: (machine precision=1e-15)*(all other unwarranted factors=10) */
+    
     if (max_j > 1.0)/* if max_j is less than 1 it won't gonna make the error lesser! */
       e *= pow(max_j,o);
-
+    if (max_f > 1.0)/* if max_f is less than 1 it won't gonna make the error lesser! */
+      e *= max_f;
     if (e == 0.0) 
       e = 1e-15;
   } 
