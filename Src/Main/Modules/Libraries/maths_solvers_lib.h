a6526edbe5baf6632b369c4684a90222973af397
Alireza Rashti <arashti2016@fau.edu>
2019-05-02 17:43:39 -0400

    Adding a New Function: Add function direct_solver_series_umfpack_dl for solving gigantic matrices one encounters in solving of equations in grid with many patches like cubed spherical.

--- Src/Main/Modules/Libraries/maths_solvers_lib.h
+++ Src/Main/Modules/Libraries/maths_solvers_lib.h
@@ -1,5 +1,6 @@
 int direct_solver_umfpack_di(void *vp);
 int direct_solver_umfpack_dl(void *vp);
 int direct_solver_series_umfpack_di(void *vp);
+int direct_solver_series_umfpack_dl(void *vp);
 void solver_tests(void);
 
