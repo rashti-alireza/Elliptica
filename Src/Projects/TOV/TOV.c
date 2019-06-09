36a80ab690f09c0434eccd2258f3f2a1e286656e
Alireza Rashti <arashti2016@fau.edu>
2019-06-09 17:52:11 -0400

    Fixing some issue for singularity at the center of star. It's not done yet.

--- Src/Projects/TOV/TOV.c
+++ Src/Projects/TOV/TOV.c
@@ -11,7 +11,7 @@
 TOV_T *TOV_solution(TOV_T *const TOV)
 {
   const double tol = 1E-5;/* tolerance in mass calculations */
-  const double Fac = 10;
+  const double Fac = 0.25;
   double h_cent_prev = 1;
   double a = 0,b = 0;
   Flag_T increase  = YES;
@@ -21,12 +21,11 @@ TOV_T *TOV_solution(TOV_T *const TOV)
   
   /* some initialization and preparation */
   TOV->calculated_baryonic_m = 0;
-  TOV->h_cent = 10;
+  TOV->h_cent = 3;
   TOV->m = alloc_double(TOV->N);
   TOV->r = alloc_double(TOV->N);
   TOV->h = alloc_double(TOV->N);
   TOV->p = alloc_double(TOV->N);
-  TOV->rbar = alloc_double(TOV->N);
   
   /* find enthalpy at the center of NS such that 
   // the baryonic mass reaches the desired value.
@@ -35,7 +34,10 @@ TOV_T *TOV_solution(TOV_T *const TOV)
   {
     solve_ODE_enthalpy_approach(TOV);
     
-    TOV->calculated_baryonic_m = calculate_baryonic_mass(TOV);
+    TOV->calculated_baryonic_m = /* test */TOV->m[TOV->N-1];
+    printf("h_cent =%g, calc m=%g\n",TOV->h_cent,TOV->calculated_baryonic_m);
+    UNUSED(calculate_baryonic_mass(TOV));
+                    
     
     /* as long as the mass is less than desired mass, increase enthalpy
     // to find the range of enthalpy */
@@ -90,8 +92,8 @@ TOV_T *TOV_solution(TOV_T *const TOV)
 /* calculate phi in g_00 = - exp[2phi] for the metric of space time */
 static void calculate_phi(TOV_T *const TOV)
 {
-  const double R = TOV->r[TOV->N],
-               M = TOV->m[TOV->N],
+  const double R = TOV->r[TOV->N-1],
+               M = TOV->m[TOV->N-1],
                *const h = TOV->h;
   unsigned i;
   TOV->phi = alloc_double(TOV->N);
@@ -144,7 +146,8 @@ static double *Komar_mass_integrand(const TOV_T *const TOV)
   EoS_T *eos = initialize_EoS();
   unsigned i;
   
-  for (i = 0; i < TOV->N; ++i)
+  f[0] = 0;
+  for (i = 1; i < TOV->N; ++i)
   {
     eos->h = h[i];
     f[i] = 4*M_PI*(eos->energy_density(eos)+3*eos->pressure(eos))*
@@ -167,7 +170,8 @@ static double *ADM_mass_integrand(const TOV_T *const TOV)
   EoS_T *eos = initialize_EoS();
   unsigned i;
   
-  for (i = 0; i < TOV->N; ++i)
+  f[0] = 0;
+  for (i = 1; i < TOV->N; ++i)
   {
     eos->h = h[i];
     f[i] = 4*M_PI*eos->energy_density(eos)*SQR(r[i])*dr_dh(h[i],r[i],m[i]);
@@ -213,11 +217,13 @@ static double *baryonic_mass_integrand(const TOV_T *const TOV)
   EoS_T *eos = initialize_EoS();
   unsigned i;
   
-  for (i = 0; i < TOV->N; ++i)
+  f[0] = 0;
+  for (i = 1; i < TOV->N; ++i)
   {
+    double s = EQL(1-2*m[i]/r[i],0) ? 0 : 1-2*m[i]/r[i];
     eos->h = h[i];
     rho = (eos->energy_density(eos)+eos->pressure(eos))/h[i];
-    f[i] = rho/sqrt(1-2*m[i]/r[i])*SQR(r[i])*dr_dh(h[i],r[i],m[i]);
+    f[i] = rho/sqrt(s)*SQR(r[i])*dr_dh(h[i],r[i],m[i]);
   }
   
   free_EoS(eos);
@@ -243,7 +249,6 @@ static void solve_ODE_enthalpy_approach(TOV_T *const TOV)
   double *const r = TOV->r;
   double *const m = TOV->m;
   double *const h = TOV->h;
-  double *const rbar = TOV->rbar;
   const double b = 1;/* h at the NS surface */
   const double a = TOV->h_cent;/* h at the center of NS */
   const double s = (b-a)/(TOV->N-1);/* step size of h. note:
@@ -252,19 +257,21 @@ static void solve_ODE_enthalpy_approach(TOV_T *const TOV)
                                // of h compare to the NS's surface. */
   double t;/* independent variable h, we conventionally called it t */
   unsigned i;
-  enum {R = 0,M = 1,Rbar = 2};
+  enum {R = 0,M = 1};
   
   /* initialization */
-  t = a;
-  r[0] = 0;/* r at center is 0 */
-  m[0] = 0;/* m at center is 0 */
   h[0] = a;/* h at center */
-  rbar[0] = 0;/* rbar at center is 0 */
+  h[1] = a+s;
+  r[0] = 0;/* r at center is 0 */
+  r[1] = r_approx(h[1],a);
+  m[0] = 0;/* m at center is 0 */
+  m[1] = m_approx(h[1],a);
+  t = h[1];
   
   /* for all points */
-  for (i = 1; i < TOV->N; ++i)
+  for (i = 2; i < TOV->N; ++i)
   {
-    double k1[3],k2[3],k3[3],k4[3];/* variables for Runge-Kutta method of 4th order */
+    double k1[2],k2[2],k3[2],k4[2];/* variables for Runge-Kutta method of 4th order */
     
     /* dr/dh and dm/dh equations: */
     k1[R] = s*dr_dh(t,r[i-1],m[i-1]);
@@ -279,29 +286,58 @@ static void solve_ODE_enthalpy_approach(TOV_T *const TOV)
     k4[R] = s*dr_dh(t+s,r[i-1]+k3[R],m[i-1]+k3[M]);
     k4[M] = s*dm_dh(t+s,r[i-1]+k3[R],m[i-1]+k3[M]);
     
-    /* isotropic coordinate transformation for drbar/dh */
-    k1[Rbar] = s*drbar_dh(t,rbar[i-1],r[i-1],m[i-1]);
-    k2[Rbar] = s*drbar_dh(t+s/2,rbar[i-1]+k1[Rbar]/2,r[i-1]+k1[R]/2,m[i-1]+k1[M]/2);
-    k3[Rbar] = s*drbar_dh(t+s/2,rbar[i-1]+k2[Rbar]/2,r[i-1]+k2[R]/2,m[i-1]+k2[M]/2);
-    k4[Rbar] = s*drbar_dh(t+s,rbar[i-1]+k3[Rbar],r[i-1]+k3[R],m[i-1]+k3[M]);
-
     /* updating the values */
     r[i] = r[i-1]+(k1[R]+2*k2[R]+2*k3[R]+k4[R])/6;
     m[i] = m[i-1]+(k1[M]+2*k2[M]+2*k3[M]+k4[M])/6;
-    h[i] = t;
-    rbar[i] = rbar[i-1]+(k1[Rbar]+2*k2[Rbar]+2*k3[Rbar]+k4[Rbar])/6;
     t = a+i*s;
+    h[i] = t;
   }
   
 }
 
+/* ->return value: approximate r near center of star */
+static double r_approx(const double h,const double h_c/* central enthalpy */)
+{
+  EoS_T *eos = initialize_EoS();
+  double r = 0;
+  double e,p,de_dh;
+  
+  eos->h = h_c;
+  e = eos->energy_density(eos);
+  p = eos->pressure(eos);
+  de_dh = eos->de_dh(eos);
+  free_EoS(eos);
+  
+  r = sqrt(3*(h_c-h)/(2*M_PI*(e+3*p)))*
+      (1-0.25*(e-3*p-3*de_dh/5)*(h_c-h)/(e+3*p));
+  
+  return r;
+}
+
+/* ->return value: approximate m near center of star */
+static double m_approx(const double h,const double h_c/* central enthalpy */)
+{
+  EoS_T *eos = initialize_EoS();
+  double m = 0;
+  double e,de_dh;
+  
+  eos->h = h_c;
+  e = eos->energy_density(eos);
+  de_dh = eos->de_dh(eos);
+  free_EoS(eos);
+  
+  m = 4*M_PI/3*e*pow(r_approx(h,h_c),3)*
+      (1-3./5.*de_dh*(h_c-h)/e);
+  
+  return m;
+}
 
 /* rbar equation:
 // \frac {d\overline {r}}{dh}=\frac {\overline {r}}{r\sqrt {1-2\frac {m}{r}}}\frac {dr}{dh} */
-static double drbar_dh(const double h,const double rbar,const double r, const double m)
-{
-  return rbar/sqrt(1-2*m/r)/r*dr_dh(h,r,m);
-}
+//static double drbar_dh(const double h,const double rbar,const double r, const double m)
+//{
+  //return rbar/sqrt(1-2*m/r)/r*dr_dh(h,r,m);
+//}
 
 /* r equation:
 // \frac {dr}{dh}=-\frac {r\left( r-2m\right) }{\left( m+4\pi r^{3}p\right) h}\\ . */
@@ -315,7 +351,6 @@ static double dr_dh(const double h,const double r, const double m)
   eos->h = h;
   p = eos->pressure(eos); 
   f = - r*(r-2*m)/(m+4*M_PI*r3*p)/h;
-  
   free_EoS(eos);
 
   return f;
