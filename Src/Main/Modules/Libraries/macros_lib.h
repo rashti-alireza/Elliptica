3778fbc4b8ddd8a3fd46c9a3ee7af2bc1dd1f089
Alireza Rashti <arashti2016@fau.edu>
2019-08-05 15:56:02 -0400

    Change notation.

--- Src/Main/Modules/Libraries/macros_lib.h
+++ Src/Main/Modules/Libraries/macros_lib.h
@@ -18,8 +18,8 @@
 #define REMOVE_FIELD(name)   remove_field(name);/* remove the field utterly */
 #define DECLARE_FIELD(name)  Field_T *const name = patch->pool[Ind(#name)];/* access to the whole field */
 #define GET_FIELD(name)      double *const name = patch->pool[Ind(#name)]->v;/* access to the memory values */
-#define PREP_FIELD(name)     Field_T *const f_##name = patch->pool[Ind(#name)];\
-                             free_coeffs(f_##name);\
+#define PREP_FIELD(name)     Field_T *const _F_##name = patch->pool[Ind(#name)];\
+                             free_coeffs(_F_##name);\
                              double *const name = patch->pool[Ind(#name)]->v;/* it frees f->v2,f->info and gets f->v. 
                                                                              // it is used to update value of a field and frees 
                                                                              // the left over of previous values */
