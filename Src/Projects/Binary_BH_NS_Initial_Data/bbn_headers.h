3b147ad1969b78314ff4edc30b72cc961ac3a3f9
Alireza Rashti <arashti2016@fau.edu>
2019-08-05 11:49:41 -0400

    Adding a general header to be used in c files produced by cpi.

---
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_headers.h
@@ -0,0 +1,11 @@
+#include "core_lib.h"
+#include "TOV_lib.h"
+#include "maths_general_lib.h"
+#include "coordinates_lib.h"
+#include "memory_managing_lib.h"
+#include "utilities_lib.h"
+#include "maths_approximation_lib.h"
+#include "physics_EoS_lib.h"
+#include "physics_StressEnergyTensor_lib.h"
+
+void bbn_update_psi10A_UiUj(Grid_T *const grid);
