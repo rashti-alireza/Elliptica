8e15c368d649dce2c5aee329beb745fc481e384d
Alireza Rashti <arashti2016@fau.edu>
2019-08-05 14:31:06 -0400

    Orginizing .math files and make sure fields get ready before update.

---
+++ Src/Main/Modules/Physics/StressEnergy_Tensor/IdealFluid_CTS_lib.h
@@ -0,0 +1,13 @@
+#include "core_lib.h"
+#include "maths_general_lib.h"
+#include "coordinates_lib.h"
+#include "memory_managing_lib.h"
+#include "utilities_lib.h"
+#include "physics_EoS_lib.h"
+
+void Tij_IF_CTS_u0(Patch_T *const patch);
+void Tij_IF_CTS_psi6J_Ui(Patch_T *const patch);
+void Tij_IF_CTS_psi6E(Patch_T *const patch);
+void Tij_IF_CTS_psi6S(Patch_T *const patch);
+void Tij_IF_CTS_psi6Sources(Grid_T *const grid);
+
