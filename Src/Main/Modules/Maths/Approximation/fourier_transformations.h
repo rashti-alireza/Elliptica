79143abe2ab32191a11ee001bde521efd9d1a5b9
Alireza Rashti <arashti2016@fau.edu>
2019-08-20 17:51:20 -0400

    Removing fftw3 header.

--- Src/Main/Modules/Maths/Approximation/fourier_transformations.h
+++ Src/Main/Modules/Maths/Approximation/fourier_transformations.h
@@ -1,6 +1,6 @@
 #include "core_lib.h"
 #include "maths_general_lib.h"
-#include <fftw3.h>
+//#include <fftw3.h>
 
 void rft_1d_ChebyshevExtrema_coeffs(double *const values ,double *const coeffs,const unsigned n);
 void rft_1d_ChebyshevNodes_coeffs(double *const values ,double *const coeffs,const unsigned n);
