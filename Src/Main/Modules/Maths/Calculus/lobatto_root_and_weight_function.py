7bb065240d57ddb48ba2c17e45c7bce650d770c4
Alireza Rashti <arashti2016@fau.edu>
2019-08-26 19:39:56 -0400

    Making lobatto root table to speed up.

--- Src/Main/Modules/Maths/Calculus/lobatto_root_and_weight_function.py
+++ Src/Main/Modules/Maths/Calculus/lobatto_root_and_weight_function.py
@@ -12,11 +12,14 @@ print (' /* direct output of this file to lobatto_root_and_weight_function.c */\
 print ('#include "core_lib.h"')
 print ('#include "maths_analytic_lib.h"')
 print ('#include "maths_general_lib.h"\n')
-print ('double Lobbatto_weight_function(const double x, const unsigned N);')
-print ('double Lobbatto_root_function(const unsigned rootN, const unsigned N);\n')
+print ('#define  ARRAY_SIZE_LOBATTO {}'.format(Nmax))
+print ('double Lobatto_root[ARRAY_SIZE_LOBATTO][ARRAY_SIZE_LOBATTO] = {{0}};\n')
 
+print ('double Lobatto_weight_function(const double x, const unsigned N);')
+print ('double Lobatto_root_function(const unsigned rootN, const unsigned N);')
+print ('void init_Lobatto_root_function(void);\n')
 print ('/* weight = 2/(n*(n-1)*(P_{n-1}(x))^2), if x == +/- 1 w = 2/(n*(n-1)) */')
-print ('double Lobbatto_weight_function(const double x, const unsigned n)')
+print ('double Lobatto_weight_function(const double x, const unsigned n)')
 print ('{')
 print ('  double fac = 2./(n*(n-1));')
 print ('  if (EQL(x,1.) || EQL(x,-1.))')
@@ -26,17 +29,22 @@ print ('}')
 
 print ('/* rootN is the n-th root of diff(legendere(N,x)). the following is like a table.')
 print ('// note: rootN starts from 0. */')
-print ('double Lobbatto_root_function(const unsigned rootN, const unsigned N)')
+print ('double Lobatto_root_function(const unsigned rootN, const unsigned N)')
 print ('{')
-print ('  double root[{}][{}] = {{0}};'.format(Nmax,Nmax))
 print ('  if (N >= {})'.format(Nmax))
 print ('    abortEr("N exceeds from the maximum.\\n");\n')
 print ('  if (rootN >= N-1)') # derivative makes it N-1 and since starts form 0, the max rootN is N-2
 print ('    abortEr("root number exceeds from the maximum.\\n");\n')
-print ('  root[0][0] = 0.;')
-print ('  root[1][0] = 0.;')
-print ('  root[2][0] = 0.;')
+print ('  return Lobatto_root[N][rootN];')
+print ('}')
 
+# table
+print ('/* init table */')
+print ('void init_Lobatto_root_function(void)')
+print ('{')
+print ('  Lobatto_root[0][0] = 0.;')
+print ('  Lobatto_root[1][0] = 0.;')
+print ('  Lobatto_root[2][0] = 0.;')
 for i in range(3,Nmax):
     root  = solve(diff(legendre(i,x),x),x,minimal=True,quick=True,warn=True)
     Realroot  = []
@@ -45,8 +53,7 @@ for i in range(3,Nmax):
       Realroot.append(re(N(root[j],precision)));
     Realroot  = sorted(Realroot)
     for j in range(Nroot):
-      print ('  root[{}][{}] = {};'.format(i,j,Realroot[j]))
+      print ('  Lobatto_root[{}][{}] = {};'.format(i,j,Realroot[j]))
 
-print ('  return root[N][rootN];')
 print ('}')
 
