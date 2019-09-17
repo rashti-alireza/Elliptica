dd9a03fcfd1c6169f6e505744f1cf8dd11d335b9
Alireza Rashti <arashti2016@fau.edu>
2019-09-17 12:00:04 -0400

    Optimizing integration by looping only on even numbers, since odd numbers will give 0. Moreover, adding a general function to integrating Chebyshev polynomial of first kind with arbitrary limits of integration.

--- Src/Main/Modules/Maths/Calculus/integration.h
+++ Src/Main/Modules/Maths/Calculus/integration.h
@@ -9,13 +9,14 @@ void free_integration(Integration_T *I);
 void plan_integration(Integration_T *const I);
 double execute_integration(Integration_T *const I);
 Integration_T *init_integration(void);
+double Int_ChebTn(const unsigned n,const unsigned N,const double xi,const double xf);
 static double Composite_Simpson_1D(Integration_T *const I);
 static double GaussQuadrature_ChebyshevExtrema(Integration_T *const I);
 static double GaussQuadrature_Lobatto(Integration_T *const I);
 static double GaussQuadrature_Legendre(Integration_T *const I);
 static double J_xyzN0N1N2(Patch_T *const patch,const unsigned ijk);
-static double Int_ChebTn(const unsigned n,const unsigned N);
 static double f_xyz_dV_Cheb_Ext_Spec(Integration_T *const I);
+static double Int_ChebTn_OPTM(const unsigned n,const unsigned N);
 
 
 
