51945f670c39b2da9c048132cf7275d66feb249c
Alireza Rashti <arashti2016@fau.edu>
2019-09-20 10:15:54 -0400

    Changing the order of enthalpy update.

--- Src/Main/Modules/Physics/StressEnergy_Tensor/Tij_IdealFluid_3plus1_decomposition.c
+++ Src/Main/Modules/Physics/StressEnergy_Tensor/Tij_IdealFluid_3plus1_decomposition.c
@@ -5,11 +5,11 @@
 
 #include "Tij_IdealFluid_3plus1_decomposition.h"
 
-/* building enthalpy, u0, _J^i, _E and _S */
+/* building u0, _J^i, _E and _S */
 void Tij_IF_CTS_psi6Sources(Grid_T *const grid)
 {
   pr_line_custom('=');
-  printf("Building enthalpy, u^0, _J^i, _E and _S ...\n");
+  printf("Building u^0, _J^i, _E and _S ...\n");
   
   unsigned p;
   
@@ -17,14 +17,13 @@ void Tij_IF_CTS_psi6Sources(Grid_T *const grid)
   {
     Patch_T *patch = grid->patch[p];
     
-    Tij_IF_CTS_enthalpy(patch);
     Tij_IF_CTS_u0(patch);
     Tij_IF_CTS_psi6J_Ui(patch);
     Tij_IF_CTS_psi6E(patch);
     Tij_IF_CTS_psi6S(patch);
   }
   
-  printf("Building enthalpy, u^0, _J^i, _E and _S ==> Done.\n");
+  printf("Building u^0, _J^i, _E and _S ==> Done.\n");
   pr_clock();
   pr_line_custom('=');
 
