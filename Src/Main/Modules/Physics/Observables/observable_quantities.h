8d3c2db14cea1d7a4437319217986b4116cf91c4
Alireza Rashti <arashti2016@fau.edu>
2019-09-26 15:37:43 -0400

    Adding a functions to prepare the integrands of ADM momentums.

--- Src/Main/Modules/Physics/Observables/observable_quantities.h
+++ Src/Main/Modules/Physics/Observables/observable_quantities.h
@@ -4,8 +4,10 @@
 #include "utilities_lib.h"
 #include "coordinates_lib.h"
 #include "maths_general_lib.h"
+#include "maths_calculus_lib.h"
 
 Observable_T *init_observable(void *grid);
 void plan_observable(Observable_T *const obs);
 static void populate_observable_BBN_CS(Observable_T *const obs);
 void free_observable(Observable_T *obs);
+void populate_ADM_momentums_integrand_PdS_GdV(const Observable_T *const obs);
