59c012225f9e8627388229fcb9d7df6cdb8ebe82
Alireza Rashti <arashti2016@fau.edu>
2019-12-11 07:50:51 -0500

    Minor.

--- Src/Projects/Binary_BH_NS_Initial_Data/bbn_XCTS_equations_lib.h
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_XCTS_equations_lib.h
@@ -2,15 +2,15 @@
 #define GET_FIELD_IF_ON_HORIZON(xNAME) \
  double *xNAME = 0;/* so it gets segfault if mistakenly the value is acquired. */\
  if (IsItHorizonPatch(patch))\
-   xNAME = patch->pool[Ind(#xNAME)]->v;
+   xNAME = patch->pool[Ind(#xNAME)]->v;\
+ UNUSED(xNAME);
 
 /* defining macro such that only gets the field value if the patch covers the NS. */
 #define GET_FIELD_IF_IN_NS(xNAME) \
  double *xNAME = 0;/* so it gets segfault if mistakenly the value is acquired. */\
  if (IsItNSPatch(patch))\
    xNAME = patch->pool[Ind(#xNAME)]->v;
-
-
+ 
 void *bbn_eq_psi(void *vp1,void *vp2);
 void *bbn_bc_psi(void *vp1,void *vp2);
 void *bbn_jacobian_eq_psi(void *vp1,void *vp2);
