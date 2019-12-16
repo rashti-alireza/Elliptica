06b2c552cb8956f75dd97c734dd6be14096c0355
Alireza Rashti <arashti2016@fau.edu>
2019-12-16 13:04:22 -0500

    Adding a function to test convergence of elliptic equations.

--- Src/Projects/Binary_BH_NS_Initial_Data/bbn_solve_initial_data_eqs.c
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_solve_initial_data_eqs.c
@@ -48,6 +48,13 @@ void bbn_solve_initial_data_eqs(Grid_T *const grid)
     /* solve equations */
     solve_eqs(SolveEqs);
     
+    /* study the solution */
+    if (strcmp_i(GetParameterS("Elliptic_Convergence_Test"),"yes"))
+    {
+      calculate_equation_residual(SolveEqs);
+      bbn_study_initial_data(grid);
+    }
+    
     /* some prints */
     printf("} Iteration %d For Solving XCTS Equations at a Fixed Resolution ==> Done.\n",iter);
     pr_clock();
