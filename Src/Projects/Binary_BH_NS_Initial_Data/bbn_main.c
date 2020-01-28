d91f4a6f7788594a37ef67e6c888caa548509ef2
Alireza Rashti <arashti2016@fau.edu>
2020-01-28 17:40:01 -0500

    Sprucing up the bbn_main function.

--- Src/Projects/Binary_BH_NS_Initial_Data/bbn_main.c
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_main.c
@@ -17,59 +17,22 @@ int Binary_BH_NS_Initial_Data(void)
   /* print some description */
   pr_clock();
   pr_line_custom('=');
-  printf("Constructing Initial Data for Binary BH and NS ...\n");
+  printf("{ Constructing Initial Data for Binary BH and NS ...\n\n");
   
   /* the outer most iteration algorithm: */
+  const unsigned N_iter = total_iterations_ip();
   Grid_T *grid_prev = 0, 
          *grid_next = 0, 
          *grid = 0;
-  const unsigned N_iter     = total_iterations_ip();
-  const unsigned N_iter_par = total_iterative_parameters_ip();
-  unsigned n[3];/* number of points */
-  const char *path_par = GetParameterS("output_directory_path");
-  char folder_name_next[1000] = {'\0'},
-       folder_name_prev[1000] = {'\0'};
-  char *folder_path,*folder_path2;
-  unsigned iter,solving_iter = 0;
+  unsigned iter;
   
   /* iterate over all iterative parameters specified in parameter file */
   for (iter = 0; iter < N_iter; ++iter)
   {
-    unsigned i;
+    printf("{ Outermost iteration %u ...\n\n",iter);
     
-    /* updating some parameters for the new round of iteration */
-    update_parameter_integer("iteration_number",(int)iter);
-    
-    /* update the parameter accoding to the iteration number */
-    update_iterative_parameter_ip(iter);
-    
-    /* making a directory for this iteration and save the path */
-    n[0] = (unsigned)GetParameterI("n_a");
-    n[1] = (unsigned)GetParameterI("n_b");
-    n[2] = (unsigned)GetParameterI("n_c");
-    
-    sprintf(folder_name_next,"BBN_%ux%ux%u",n[0],n[1],n[2]);
-    if (strcmp(folder_name_next,folder_name_prev))/* if n is updated */
-    {
-      /* iteration number used in solving, reset this for each resolution */
-      solving_iter = 0;
-      update_parameter_integer("solving_iteration_number",(int)solving_iter);
-      sprintf(folder_name_next,"BBN_%ux%ux%u",n[0],n[1],n[2]);
-      sprintf(folder_name_prev,"BBN_%ux%ux%u",n[0],n[1],n[2]);
-      folder_path = make_directory(path_par,folder_name_next);
-      update_parameter_string("iteration_output",folder_path);
-      folder_path2 = make_directory(folder_path,"Diagnostics");
-      update_parameter_string("Diagnostics",folder_path2);
-      free(folder_path);
-      free(folder_path2);
-    }
-    
-    printf("{ Iteration %u for the parameter(s) below:\n",iter);
-    pr_parameters();/* printing in the folder */
-    for (i = 0; i < N_iter_par; ++i)
-    {
-      printf("%-10s = %-10s\n",par_name_ip(i),par_value_str_ip(i));
-    }
+    /* update iterative parameters and directories */
+    update_parameters_and_directories(iter);
     
     /* preparing fields and grid according to the given previous grid */
     grid_next = bbn_initialize_next_grid(grid_prev);
@@ -88,11 +51,7 @@ int Binary_BH_NS_Initial_Data(void)
     
     grid_prev = grid_next;
     
-    printf("} Iteration %u for the parameter(s) below is done.\n",iter);
-    for (i = 0; i < N_iter_par; ++i)
-    {
-      printf("%-10s = %-10s\n",par_name_ip(i),par_value_str_ip(i));
-    }
+    printf("} Outermost iteration %u --> Done.\n",iter);
   }
   grid = grid_next;/* final grid */
   
@@ -100,7 +59,7 @@ int Binary_BH_NS_Initial_Data(void)
   free_grid(grid);
     
   /* print some description */
-  printf("Constructing Initial Data for Binary BH and NS ==> Done. :)\n");
+  printf("} Constructing Initial Data for Binary BH and NS ==> Done. :)\n");
   pr_clock();
   pr_line_custom('=');
 
@@ -119,56 +78,19 @@ static void Elliptic_Eqs_Convergence_Test_BBN(void)
   printf("Convergence test of elliptic equations for binary BH-NS ...\n\n");
   
   /* the outer most iteration algorithm: */
+  const unsigned N_iter = total_iterations_ip();
   Grid_T *grid_prev = 0, 
          *grid_next = 0, 
          *grid = 0;
-  const unsigned N_iter     = total_iterations_ip();
-  const unsigned N_iter_par = total_iterative_parameters_ip();
-  unsigned n[3];/* number of points */
-  const char *path_par = GetParameterS("output_directory_path");
-  char folder_name_next[1000] = {'\0'},
-       folder_name_prev[1000] = {'\0'};
-  char *folder_path,*folder_path2;
-  unsigned iter,solving_iter = 0;
+  unsigned iter;
   
   /* iterate over all parameters specified in parameter file */
   for (iter = 0; iter < N_iter; ++iter)
   {
-    unsigned i;
+    printf("{ Outermost iteration %u ...\n\n",iter);
     
-    /* updating some parameters for the new round of iteration */
-    update_parameter_integer("iteration_number",(int)iter);
-    
-    /* update the parameter accoding to the iteration number */
-    update_iterative_parameter_ip(iter);
-    
-    /* making a directory for this iteration and save the path */
-    n[0] = (unsigned)GetParameterI("n_a");
-    n[1] = (unsigned)GetParameterI("n_b");
-    n[2] = (unsigned)GetParameterI("n_c");
-    
-    sprintf(folder_name_next,"BBN_%ux%ux%u",n[0],n[1],n[2]);
-    if (strcmp(folder_name_next,folder_name_prev))/* if n is updated */
-    {
-      /* iteration number used in solving, reset this for each resolution */
-      solving_iter = 0;
-      update_parameter_integer("solving_iteration_number",(int)solving_iter);
-      sprintf(folder_name_next,"BBN_%ux%ux%u",n[0],n[1],n[2]);
-      sprintf(folder_name_prev,"BBN_%ux%ux%u",n[0],n[1],n[2]);
-      folder_path = make_directory(path_par,folder_name_next);
-      update_parameter_string("iteration_output",folder_path);
-      folder_path2 = make_directory(folder_path,"Diagnostics");
-      update_parameter_string("Diagnostics",folder_path2);
-      free(folder_path);
-      free(folder_path2);
-    }
-    
-    printf("{ Iteration %u for the parameter(s) below:\n",iter);
-    pr_parameters();/* printing in the folder */
-    for (i = 0; i < N_iter_par; ++i)
-    {
-      printf("%-10s = %-10s\n",par_name_ip(i),par_value_str_ip(i));
-    }
+    /* update iterative parameters and directories */
+    update_parameters_and_directories(iter);
     
     /* preparing fields and grid according to the given previous grid */
     grid_next = bbn_initialize_next_grid(grid_prev);
@@ -183,12 +105,8 @@ static void Elliptic_Eqs_Convergence_Test_BBN(void)
     bbn_study_initial_data(grid_next);
     
     grid_prev = 0;
-    
-    printf("} Iteration %u for the parameter(s) below is done.\n",iter);
-    for (i = 0; i < N_iter_par; ++i)
-    {
-      printf("%-10s = %-10s\n",par_name_ip(i),par_value_str_ip(i));
-    }
+  
+    printf("} Outermost iteration %u --> Done.\n",iter);  
   }
   grid = grid_next;/* final grid */
   
@@ -199,4 +117,51 @@ static void Elliptic_Eqs_Convergence_Test_BBN(void)
   printf("\nConvergence test of elliptic equations for binary BH-NS ==> Done. :)\n");
   pr_clock();
   pr_line_custom('=');
+}
+
+/* updating iterative parametes and output directories */
+static void update_parameters_and_directories(const unsigned iter)
+{
+  const unsigned N_iter_par = total_iterative_parameters_ip();
+  unsigned n[3];/* number of points */
+  const char *path_par = GetParameterS("output_directory_path");
+  char folder_name_next[1000] = {'\0'},
+       folder_name_prev[1000] = {'\0'};
+  char *folder_path,*folder_path2;
+  unsigned i;
+    
+  /* updating some parameters for the new round of iteration */
+  update_parameter_integer("iteration_number",(int)iter);
+  
+  /* update the parameter accoding to the iteration number */
+  update_iterative_parameter_ip(iter);
+  
+  /* print the iterative parameters */
+  if (N_iter_par)
+  {
+    printf("Iterative parameters:\n");
+    for (i = 0; i < N_iter_par; ++i)
+      printf("|--> %-30s = %-15s\n",par_name_ip(i),par_value_str_ip(i));
+    printf("\n");
+  }
+  
+  /* making a directory for this iteration and save the path */
+  n[0] = (unsigned)GetParameterI("n_a");
+  n[1] = (unsigned)GetParameterI("n_b");
+  n[2] = (unsigned)GetParameterI("n_c");
+  
+  sprintf(folder_name_next,"BBN_%ux%ux%u",n[0],n[1],n[2]);
+  if (strcmp(folder_name_next,folder_name_prev))/* if n is updated */
+  {
+    /* iteration number used in solving, reset this for each resolution */
+    update_parameter_integer("solving_iteration_number",0);
+    sprintf(folder_name_next,"BBN_%ux%ux%u",n[0],n[1],n[2]);
+    sprintf(folder_name_prev,"BBN_%ux%ux%u",n[0],n[1],n[2]);
+    folder_path = make_directory(path_par,folder_name_next);
+    update_parameter_string("iteration_output",folder_path);
+    folder_path2 = make_directory(folder_path,"Diagnostics");
+    update_parameter_string("Diagnostics",folder_path2);
+    free(folder_path);
+    free(folder_path2);
+  }
 }
