dc3d75976c3c7bb2b3e6fad7341cc88c7440e3ea
Alireza Rashti <arashti2016@fau.edu>
2020-01-31 12:35:01 -0500

    Minor

--- Src/Main/Cores/parameters.c
+++ Src/Main/Cores/parameters.c
@@ -63,7 +63,7 @@ void update_parameter_double_format(const char *const lv, const double rv)
     printf("         |--> parameter     = %s\n",lv);
     printf("         |--> new value     = %+g\n",rv);
     printf("         |--> old value     = %+g\n",par->rv_double);
-    printf("         |--> v_new - v_old = %+g\n\n",rv-par->rv_double);
+    printf("         |--> v_new - v_old = %+g\n",rv-par->rv_double);
     
     sprintf(str_rv,"%15.18f",rv);
     _free(par->rv);
