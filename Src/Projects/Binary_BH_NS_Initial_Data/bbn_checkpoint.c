49ec85ef91e5e29fd0593d9c8a1d81c5a86ca9a7
Alireza Rashti <arashti2016@fau.edu>
2020-02-11 09:48:16 -0500

    Adding some checks and commands for files.

--- Src/Projects/Binary_BH_NS_Initial_Data/bbn_checkpoint.c
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_checkpoint.c
@@ -12,6 +12,7 @@ void bbn_write_checkpoint(const Grid_T *const grid)
   pr_line_custom('=');
   printf("{ Writing checkpoint ...\n");
   
+  const char *const checkpoint_file_name = "checkpoint.dat"
   FILE *checkpoint_file = 0;
   const double dt  = Pgetd("checkpoint_dt_hours");
   const double now = get_time_sec()/(3600);
@@ -41,28 +42,44 @@ void bbn_write_checkpoint(const Grid_T *const grid)
   last_checkpoint_was = now;
   
   /* open checkpoint file */
-  checkpoint_file = open_binary_file("checkpoint.BIN")
+  //checkpoint_file = open_binary_file("checkpoint.BIN")
   
   /* write all parameters in the checkpoint file */
-  write_parameters(grid,checkpoint_file);
+  write_parameters(grid,checkpoint_file_name);
   
   /* write all fields value in the checkpoint file */
-  write_fields(grid,checkpoint_file);
+  write_fields(grid,checkpoint_file_name);
   
-  /* close and replace checkpoint file */
-  close_binary_file(checkpoint_file,"checkpoint.BIN");
+  /* replace checkpoint file with the previous */
+  move_binary_file(checkpoint_file_name);
   
   printf("} Writing checkpoint ==> Done.\n");
   pr_clock();
   pr_line_custom('=');
 }
 
-/* open checkpoint file with the given name fname */
-static void *open_binary_file(const char *const fname)
+/* replace checkpoint file with the previous one */
+static void move_checkpoint_file(const char *const file_name)
 {
+  const char *const folder = Pgets("iteration_output");
+  char file_path[MAX_ARR];
+  char command[2*MAX_ARR];
+  
+  sprintf(file_path,"%s/%s",folder,file_name);
+  sprintf(command,"mv %s/%s_temp %s/%s",
+          folder,file_name,folder,file_name);
+  printf("shell command:\n$ mv %s_temp %s\n\n",file_name,file_name);
+  fflush(stdout);
+  system(command);
+}
+
+/* write all of the pertinent parameters in the checkpoint file */
+static void write_parameters(const Grid_T *const grid,FILE *const file)
+{
+  printf ("~> Writing all parameters in the checkpoint file ...\n");
   FILE *file = 0;
   const char *const folder = Pgets("iteration_output");
-  char file_path[100];
+  char file_path[MAX_ARR];
   
   sprintf(file_path,"%s/%s_temp",folder,fname);
   if (access(file_path,F_OK) != -1)/* if file exists */
@@ -71,18 +88,36 @@ static void *open_binary_file(const char *const fname)
   file = fopen(file_path,"wb");
   pointerEr(file);
   
-  return file;
-}
+  /* write parameters */
+  unsigned np;
 
-/* write all of the pertinent parameters in the checkpoint file */
-static void write_parameters(const Grid_T *const grid,FILE *const file)
-{
-  printf ("~> Writing all parameters in the checkpoint file ...\n");
+  np = 0;
+  while (parameters_global != 0 && parameters_global[np] != 0)
+  {
+    Parameter_T *par = parameters_global[np];
+    fwrite(par,sizeof(*par),1,file);
+  }
+  
+  fclose(file);
 }
 
 /* write all of the fields in the checkpoint file */
 static void write_fields(const Grid_T *const grid,FILE *const file)
 {
   printf ("~> Writing all fields in the checkpoint file ...\n");
+  FILE *file = 0;
+  const char *const folder = Pgets("iteration_output");
+  char file_path[MAX_ARR];
+  
+  sprintf(file_path,"%s/%s_temp",folder,fname);
+  if (access(file_path,F_OK) != -1)/* if file exists */
+  {
+    file = fopen(file_path,"w");
+    pointerEr(file);
+  }
+  else
+    abortEr("File does not already exist.\n");
+    
+  fclose(file);  
 }
 
--- Src/Projects/Binary_BH_NS_Initial_Data/bbn_checkpoint.h
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_checkpoint.h
@@ -1,5 +1,8 @@
 #include "core_lib.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
+extern Parameter_T **parameters_global;
 
 void bbn_write_checkpoint(const Grid_T *const grid);
