038c380ce3eec2afe66cec5b6eccc5b0974d2798
Alireza Rashti <arashti2016@fau.edu>
2020-02-18 15:16:02 -0500

    Sprucing up.

--- Src/Projects/Poisson/Laplace_Inhom/laplace_inhom_analyze.h
+++ Src/Projects/Poisson/Laplace_Inhom/laplace_inhom_analyze.h
@@ -2,7 +2,7 @@
 #include "manifold_lib.h"
 #include "macros_lib.h"
 #include "maths_general_lib.h"
-#include "maths_field_analysis.h"
+#include "maths_field_analysis_lib.h"
 #include "fields_lib.h"
 
 int Laplace_Inhom_analyze_answer(const Grid_T *const grid);
