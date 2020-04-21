f58d72f0fa75de97c051d6f4d749b42e7acfefc6
Alireza Rashti <arashti2016@fau.edu>
2020-04-21 16:18:33 -0400

    Changing the interface of functions.

--- Src/Main/Modules/Physics/Observables/observable_quantities.h
+++ Src/Main/Modules/Physics/Observables/observable_quantities.h
@@ -1,20 +1,9 @@
 #include "core_lib.h"
 #include "physics_observables_lib.h"
 #include "utilities_lib.h"
-#include "manifold_lib.h"
-#include "maths_general_lib.h"
-#include "maths_calculus_lib.h"
-#include "fields_lib.h"
 
-Observable_T *init_observable(void *grid);
+Observable_T *init_observable(void *grid,void (*plan_items)(struct OBSERVABLE_T *obs),void (*free_items)(struct OBSERVABLE_T *obs));
 void plan_observable(Observable_T *const obs);
-static void populate_observable_BBN_CS(Observable_T *const obs);
 void free_observable(Observable_T *obs);
-void populate_ADM_momentums_integrand_PdS_GdV(const Observable_T *const obs);
-static double ADM_momentum_x_BBN_CS(Observable_T *const obs);
-static double ADM_momentum_y_BBN_CS(Observable_T *const obs);
-static double ADM_momentum_z_BBN_CS(Observable_T *const obs);
-static double ADM_angular_momentum_x_BBN_CS(Observable_T *const obs);
-static double ADM_angular_momentum_y_BBN_CS(Observable_T *const obs);
-static double ADM_angular_momentum_z_BBN_CS(Observable_T *const obs);
+
 
