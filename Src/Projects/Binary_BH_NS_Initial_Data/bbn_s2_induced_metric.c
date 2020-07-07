418ece437a0e556cf89462fb5f07ea4ca7e83caa
Alireza Rashti <arashti2016@fau.edu>
2020-07-07 15:07:45 -0400

    Adding more stuff for interpolation and population of the induced metric.

--- Src/Projects/Binary_BH_NS_Initial_Data/bbn_s2_induced_metric.c
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_s2_induced_metric.c
@@ -10,14 +10,26 @@ bbn_compute_induced_metric_on_S2_CS_Ylm_CTS
   Grid_T *const grid,
   const char *const type,/* NS or BH */
   const unsigned lmax,/* l max in Ylm */
-  double *const h_D0D0,/* induced h00  */
-  double *const h_D0D1,/* induced h01  */
-  double *const h_D1D1 /* induced h11  */
+  double **const ph_D0D0,/* induced h00  pointer */
+  double **const ph_D0D1,/* induced h01  pointer */
+  double **const ph_D1D1 /* induced h11  pointer */
   )
 {
+  const unsigned N = Pow2(2*lmax+1);
+  double *const h_D0D0 = alloc_double(N);
+  double *const h_D0D1 = alloc_double(N);
+  double *const h_D1D1 = alloc_double(N);
   double theta,phi;
   unsigned Ntheta,Nphi;
+  int type_flg = -1;/* 1 for NS , 0 for BH */
   
+  if (strcmp_i(type,"BH"))
+    type_flg = 0;
+  else if (strcmp_i(type,"NS"))
+    type_flg = 1;
+  else
+    Error0("Bad argument: no such type.");
+    
   /* initialize tables */
   init_Legendre_root_function();
   Ntheta  = Nphi = 2*lmax+1;
@@ -30,43 +42,61 @@ bbn_compute_induced_metric_on_S2_CS_Ylm_CTS
     {
       phi = j*2*M_PI/Nphi;
       
+      unsigned ij = IJ(i,j);
       Patch_T *patch = 0;
+      double ipsi,
+            i_gamma_D0D0,i_gamma_D0D1,i_gamma_D0D2,
+            i_gamma_D1D1,i_gamma_D1D2,i_gamma_D2D2,
+            gamma_D0D0,gamma_D0D1,gamma_D0D2,
+            gamma_D1D1,gamma_D1D2,gamma_D2D2;
       /* find patch and X,Y,Z on the surface in which theta and phi take place */
       find_XYZ_and_patch_of_theta_phi_CS(X,&patch,theta,phi,grid,type);
 
-      /* find enthalpy at the (X,Y,Z) */
-      Interpolation_T *interp_psi = init_interpolation();
-      interp_h->field = patch->pool[Ind("psi")];
-      interp_h->XY_dir_flag  = 1;
-      interp_h->X            = X[0];
-      interp_h->Y            = X[1];
-      interp_h->K            = patch->n[2]-1;
-      plan_interpolation(interp_h);
-      h = execute_interpolation(interp_h);/* enthalpy */
-
-      assert(h > 0);
-
-      free_interpolation(interp_h);
-
+      /* find value at the (X,Y,Z) */
+      INTERPOLATE_macro(psi);
+      INTERPOLATE_macro(_gamma_D0D0);
+      INTERPOLATE_macro(_gamma_D0D1);
+      INTERPOLATE_macro(_gamma_D0D2);
+      INTERPOLATE_macro(_gamma_D1D1);
+      INTERPOLATE_macro(_gamma_D1D2);
+      INTERPOLATE_macro(_gamma_D2D2);
       
+      /* gamma = psi * _gamma */
+      gamma_D0D0 = ipsi*i_gamma_D0D0;
+      gamma_D0D1 = ipsi*i_gamma_D0D1;
+      gamma_D0D2 = ipsi*i_gamma_D0D2;
+      gamma_D1D1 = ipsi*i_gamma_D1D1;
+      gamma_D1D2 = ipsi*i_gamma_D1D2;
+      gamma_D2D2 = ipsi*i_gamma_D2D2;
+      
+      //bbn_populate_3d_included_metric_S2_theta_phi
+      bbn_populate_2d_induced_metric_S2_theta_phi(
+         &h_D0D0[ij],&h_D0D1[ij],&h_D1D1[ij],
+         &gamma_D0D0,&gamma_D0D1,&gamma_D0D2
+         &gamma_D1D1,&gamma_D1D2,&gamma_D2D2,
+         theta,phi);
       
     }/* end of for (j = 0; j < Nphi; ++j) */
   }/* end for (i = 0; i < Ntheta; ++i) */
 
+  *ph_D0D0 = h_D0D0;
+  *ph_D0D1 = h_D0D1;
+  *ph_D1D1 = h_D1D1;
+  
 }
 
 /* given theta, phi on the surface and type (BH or NS)
 // it finds the corresponding patch and X,Y,Z coordinate. */
 static void 
 find_XYZ_and_patch_of_theta_phi_CS
-                                  (
-                                  double *const X,/* fill X coords */
-                                  Patch_T **const ppatch,/* fill patch */
-                                  const double theta,/* (theta,phi) */
-                                  const double phi,/* (theta,phi) */
-                                  Grid_T *const grid,/* in this grid */
-                                  const char *const type/* [BH,NS] */
-                                  )
+  (
+  double *const X,/* fill X coords */
+  Patch_T **const ppatch,/* fill patch */
+  const double theta,/* (theta,phi) */
+  const double phi,/* (theta,phi) */
+  Grid_T *const grid,/* in this grid */
+  const char *const type/* [BH,NS] */
+  )
 {
   const double tan_phi    = tan(phi);
   const double cos_theta  = cos(theta);
--- Src/Projects/Binary_BH_NS_Initial_Data/bbn_s2_induced_metric.h
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_s2_induced_metric.h
@@ -1,3 +1,25 @@
+#include "bbn_headers.h"
 
+
+#define IJ(i,j) ((j)+Nphi*(i))
+
+/* getting field name (xName) and put the interpolant into ixName */
+#define INTERPOLATE_macro(xName) \
+  { \
+    Interpolation_T *interp_##xName = init_interpolation(); \
+    interp_##xName->field = patch->pool[Ind(#xName)]; \
+    interp_##xName->XY_dir_flag  = 1; \
+    interp_##xName->X            = X[0]; \
+    interp_##xName->Y            = X[1];\ 
+    interp_##xName->K            = type_flg == 1/* NS? */? patch->n[2]-1 : 0; \
+    plan_interpolation(interp_##xName); \
+    i##xName = execute_interpolation(interp_##xName); \
+    free_interpolation(interp_##xName); \
+  }
 
 static void find_XYZ_and_patch_of_theta_phi_CS(double *const X,Patch_T **const ppatch,const double theta,const double phi,Grid_T *const grid,const char *const type);
+void bbn_populate_2d_induced_metric_S2_theta_phi(double *const h_D0D0,double *const h_D0D1,double *const h_D1D1,const double *const g_D0D0,const double *const g_D0D1,const double *const g_D0D2,const double *const g_D1D1,const double *const g_D1D2,const double *const g_D2D2,const double theta,const double phi);
+void bbn_populate_3d_included_metric_S2_theta_phi(const double *const h_D0D0,const double *const h_D0D1,const double *const h_D1D1,double *const g_D0D0, double *const g_D0D1, double *const g_D0D2, double *const g_D1D1, double *const g_D1D2, double *const g_D2D2, double theta,const double phi);
+
+
+
