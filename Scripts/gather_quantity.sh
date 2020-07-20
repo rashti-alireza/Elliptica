81cdf83d663873d1b42604ae0262910c7f3b8797
Alireza Rashti <arashti2016@fau.edu>
2020-07-20 09:36:27 -0400

    Adding a feature to scale the collected quantity in order to compare different quantities which are at different scales.

--- Scripts/gather_quantity.sh
+++ Scripts/gather_quantity.sh
@@ -22,7 +22,8 @@
 # Note: This script tends to mainly use *_geometry_and_physics.txt files.
 #
 # usage:
-# $gather_quantity.sh quantity_name file_name
+# $collect.sh quantity_name file_name    # collect and plot
+# $collect.sh -s quantity_name file_name # collect, scale and plot
 #
 
 #!/bin/bash
@@ -33,13 +34,22 @@ prefix=${argv[0]}
 quant="\b${argv[0]}\b"
 file=${argv[1]}
 out=${prefix}_${file}
+scale=0
 
 # check if it needs help
 if [[ $argc -le 1 || $1 =~ --h.? ]]; then
         printf \
 "usage:\n"\
-"$ gather_quantity.sh quantity_name file_name\n"
+"$ collect.sh quantity_name file_name    # --> (collect and plot)\n"\
+"$ collect.sh -s quantity_name file_name # --> (collect, scale and plot)\n"
 	exit 1
+# if asked scaling
+elif [[ $1 =~ -s.? ]]; then
+	scale=1
+	file=${argv[2]}
+	prefix=${argv[1]}
+	quant="\b${argv[1]}\b"
+	out=scaled_${prefix}_${file}
 fi
 
 # check if file exists
@@ -50,9 +60,41 @@ then
 fi
 
 # now make the table:
-printf "# iteration %s\n" ${quant} > ${out}
-grep -i -E ${quant} ${file} | \
-awk 'BEGIN{v=0;}{ v = v+1; print v " " $3}' >> ${out} ;
+printf "# iteration %s\n" ${prefix} > ${out}
+
+# if scale required
+if [[ ${scale} -eq 1 ]];then
+	data=$(grep -i -E ${quant} ${file} | \
+		sed 's/+//g' | \
+		awk '{print $3}'| tr '\n' ',')
+	IFS=',' read -r -a array <<< "${data}"
+	
+	# find the max to scale
+	max=0
+	for (( i=0; i < ${#array[@]}; ++i ));
+	do
+		r=${array[i]}
+		if (( $(echo "$r < 0" | bc -l) ));
+	        then
+        	 	r=$(echo "$r * (-1)" | bc -l)
+        	fi
+        	
+        	if (( $(echo "$r > $max" | bc -l) ));
+        	then
+        		max=$r
+        	fi
+	done
+	# scale by dividing by max and print
+	for (( i=0; i < ${#array[@]}; ++i ));
+	do
+		array[i]=$(echo "${array[i]}/${max}" | bc -l)
+		echo $((i+1)) ${array[i]} >> ${out}
+	done
+# just print to file
+else
+	grep -i -E ${quant} ${file} | \
+	awk 'BEGIN{v=0;}{ v = v+1; print v " " $3}' >> ${out} ;
+fi
 
 # print:
 printf "\n%s  = %s\n\n" "==> output file" ${out}
