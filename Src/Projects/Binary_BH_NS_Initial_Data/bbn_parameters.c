fa35dd64cbe48d3d62b80fabe36920bab2083998
Alireza Rashti <arashti2016@fau.edu>
2020-09-22 17:27:52 -0400

    Remove redundant par.

--- Src/Projects/Binary_BH_NS_Initial_Data/bbn_parameters.c
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_parameters.c
@@ -124,7 +124,6 @@ void bbn_set_default_parameters(void)
   // 2. Steepest_Descent: using steepest descent method. */
   Pset_default("NS_surface_root_finder","Bisect_Single");
  
- /* BH-filler parameter: max l in Ylm expansion */ 
- Pset_default("bbn_bhfiller_lmax","10");
- 
 }
+
+
