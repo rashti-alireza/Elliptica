e15573655e29e8e812ceacf056b736663dca26bf
Alireza Rashti <arashti2016@fau.edu>
2020-09-22 14:24:32 -0400

    Improve coeffs of ChebTn filler.

--- Src/Projects/Binary_BH_NS_Initial_Data/bbn_bhf_ChebTn_filler.c
+++ Src/Projects/Binary_BH_NS_Initial_Data/bbn_bhf_ChebTn_filler.c
@@ -8,34 +8,30 @@ void bbn_bhf_ChebTn_extrapolate
 (double *const a,const double fr0,const double fr1,const double dfdr,const double ddfddr,const double rfill)
 {
 a[0] = 
-(143.0/131072.0)*ddfddr*pow(rfill, 2) - 1859.0/65536.0*dfdr*rfill + 
-(20899.0/32768.0)*fr0 + (11869.0/32768.0)*fr1;
+(1263.0/131072.0)*ddfddr*pow(rfill, 2) - 5359.0/65536.0*dfdr*rfill + 
+(1.0/2.0)*fr0 + (1.0/2.0)*fr1;
 a[1] = 
-(143.0/131072.0)*ddfddr*pow(rfill, 2) - 2145.0/65536.0*dfdr*rfill - 
-9009.0/16384.0*fr0 + (9009.0/16384.0)*fr1;
+(521.0/131072.0)*ddfddr*pow(rfill, 2) - 3461.0/65536.0*dfdr*rfill - 
+19845.0/32768.0*fr0 + (19845.0/32768.0)*fr1;
 a[2] = 
--13.0/16384.0*ddfddr*pow(rfill, 2) + (91.0/8192.0)*dfdr*rfill - 819.0/
-4096.0*fr0 + (819.0/4096.0)*fr1;
+(1.0/16384.0)*rfill*(-223*ddfddr*rfill + 1470*dfdr);
 a[3] = 
--91.0/65536.0*ddfddr*pow(rfill, 2) + (1001.0/32768.0)*dfdr*rfill + 
-(273.0/8192.0)*fr0 - 273.0/8192.0*fr1;
+-441.0/65536.0*ddfddr*pow(rfill, 2) + (2205.0/32768.0)*dfdr*rfill + 
+(2205.0/16384.0)*fr0 - 2205.0/16384.0*fr1;
 a[4] = 
--21.0/32768.0*ddfddr*pow(rfill, 2) + (329.0/16384.0)*dfdr*rfill + 
-(567.0/8192.0)*fr0 - 567.0/8192.0*fr1;
+(147.0/32768.0)*rfill*(ddfddr*rfill - 2*dfdr);
 a[5] = 
-(9.0/65536.0)*ddfddr*pow(rfill, 2) + (133.0/32768.0)*dfdr*rfill + 
-(189.0/8192.0)*fr0 - 189.0/8192.0*fr1;
+(219.0/65536.0)*ddfddr*pow(rfill, 2) - 567.0/32768.0*dfdr*rfill - 
+567.0/16384.0*fr0 + (567.0/16384.0)*fr1;
 a[6] = 
-(5.0/16384.0)*ddfddr*pow(rfill, 2) - 19.0/8192.0*dfdr*rfill - 21.0/
-4096.0*fr0 + (21.0/4096.0)*fr1;
+(9.0/16384.0)*rfill*(-ddfddr*rfill + 2*dfdr);
 a[7] = 
-(41.0/262144.0)*ddfddr*pow(rfill, 2) - 239.0/131072.0*dfdr*rfill - 
-207.0/32768.0*fr0 + (207.0/32768.0)*fr1;
+-169.0/262144.0*ddfddr*pow(rfill, 2) + (405.0/131072.0)*dfdr*rfill + 
+(405.0/65536.0)*fr0 - 405.0/65536.0*fr1;
 a[8] = 
-(5.0/131072.0)*ddfddr*pow(rfill, 2) - 33.0/65536.0*dfdr*rfill - 63.0/
-32768.0*fr0 + (63.0/32768.0)*fr1;
+(5.0/131072.0)*rfill*(ddfddr*rfill - 2*dfdr);
 a[9] = 
-(1.0/262144.0)*ddfddr*pow(rfill, 2) - 7.0/131072.0*dfdr*rfill - 7.0/
-32768.0*fr0 + (7.0/32768.0)*fr1;
+(15.0/262144.0)*ddfddr*pow(rfill, 2) - 35.0/131072.0*dfdr*rfill - 35.0/
+65536.0*fr0 + (35.0/65536.0)*fr1;
 }
 
