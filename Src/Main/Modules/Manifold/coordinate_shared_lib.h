03b5fb18e143fc7af3bf4bcf57edf38a32736e8d
Alireza Rashti <arashti2016@fau.edu>
2020-10-06 19:35:02 -0400

    Improving macro definition.

--- Src/Main/Modules/Manifold/coordinate_shared_lib.h
+++ Src/Main/Modules/Manifold/coordinate_shared_lib.h
@@ -15,29 +15,33 @@
 
 /* some conventions: */
 /* split cubed spherical suffix */
-static const char *const scs_suffix = "X%uY%uZ%u";
+#define SCS_suffix "X%uY%uZ%u"
 
-/* prototype for surface function, ex: grid1_left_NS_sigmaU_back023 */
-static const char *const scs_prototype_sigma = "grid%u_%s_%s_%s_%s%u%u%u";
-
-/* surface function sprintf NOTE: d0,d1,d2 is required */
-#define scs_wpar_sigma(Sstr,Ugridnumber,Sdirection,Sobject,Fside,Ssigma) \
-  assert(sprintf(Sstr,scs_prototype_sigma,(Ugridnumber),Ssigma,\
-         Sdirection,Sobject,StrSide[Fside],d0,d1,d2));
+/* par format for surface function.
+// NOTE: d0,d1,d2,grid,dir,obj and side are required.
+// ex: grid1_sigmaU_left_NS_backX0Y2Z3 */
+#define SCS_par_sigma(par,sigma) \
+  sprintf(par,"grid%u_%s_%s_%s_%s"SCS_suffix,\
+  grid->gn,sigma,dir,obj,StrSide[side],d0,d1,d2);
 
 /* surface function stem */
 #define SigmaU "sigmaU"
 #define SigmaD "sigmaD"
 
-/* center of patch with respect to the reference Cartesian coords. */
-#define scs_wpar_center(Sstr,Ugridnumber,Sdirection,Sobject,Svar) \
-  assert(sprintf(Sstr,"grid%u_%s_%s_center_%s",(Ugridnumber),\
-         Sdirection,Sobject,Svar));
+/* par format for center of patch with respect to the 
+// reference Cartesian coords. 
+// NOTE: grid,dir,obj and side are required.
+// ex: grid1_left_BH_front_center_a */
+#define SCS_par_center(par,axis) \
+  sprintf(par,"grid%u_%s_%s_center_%s_%s",\
+  grid->gn,dir,obj,StrSide[side],axis);
 
-/* box lengths */
-#define scs_wpar_box(Spar,Ugridnumber,Sdirection,Sobject,Svar) \
-  assert(sprintf(Spar,"grid%u_%s_%s_%s%u%u%u",(Ugridnumber),\
-         Sdirection,Sobject,Svar,d0,d1,d2));
+/* par format for box lengths.
+// NOTE: d0,d1,d2,grid,dir and obj are required.
+// ex: grid1_central_box_len_a_leftX0Y2Z3 */
+#define SCS_par_boxlen(par,axis) \
+  sprintf(par,"grid%u_%s_len_%s_%s"SCS_suffix,\
+  grid->gn,obj,axis,dir,d0,d1,d2);
 
 /* sides, NOTE: the order is important, it MUST be like FLAG_T */
 static const char *const StrSide[] = 
