fae5f5560f78d58d0cc6d24333cb5ee93020d470
Alireza Rashti <arashti2016@fau.edu>
2020-10-07 09:04:03 -0400

    Adding new Flag_T.

--- Src/Main/Core/typdef_data.h
+++ Src/Main/Core/typdef_data.h
@@ -18,7 +18,7 @@
 /* common flags*/
 typedef enum FLAG_T
 {
-  UNDEFINED = 0,
+  UNDEFINED = -1,
   NONE,
   NO,
   YES,
@@ -40,6 +40,8 @@ typedef enum FLAG_T
   SR_T_CS,/* surrounding type in cubed spherical */
   OT_T1_CS,/* outermost type1 in cubed spherical */
   OT_T2_CS,/* outermost type2 in cubed spherical */
+  OJ_T_SCS,/* object type in split cubed spherical eg:NS/BH_surrounding */
+  OT_T_SCS,/* outermost type in split cubed spherical (compactified) */
   NOT_INITIALIZE
 }Flag_T;
 
