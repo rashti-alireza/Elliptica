276fe4fe1e88820641fd8c4e3bb656acbb904be3
Alireza Rashti <arashti2016@fau.edu>
2020-10-07 15:03:25 -0400

    Better example.

--- Scripts/visit_plot_mesh.py
+++ Scripts/visit_plot_mesh.py
@@ -4,7 +4,9 @@
 #
 # 1. with windows:
 # $ visit -cli -s visit_plot_mesh.py "regex name of file name" --path .
-# ex: $ visit -cli -s visit_plot_mesh.py "left_central_box_left_X.Y.Z..*"
+# ex: 
+# $ visit -cli -s visit_plot_mesh.py \
+#              "(left_central_box_left|left_NS_Up)_X.Y.Z.*"
 # 
 # 2. without windows: 
 # $ visit -nowin -cli -s visit_plot_mesh.py "regex name of file name" --path .
