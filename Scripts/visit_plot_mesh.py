5dd4d2b65aace972f0a11ea22ab1fb40beb1e36e
Alireza Rashti <arashti2016@fau.edu>
2020-10-13 17:03:11 -0400

    Option for mesh coloring.

--- Scripts/visit_plot_mesh.py
+++ Scripts/visit_plot_mesh.py
@@ -18,6 +18,11 @@ import re
 import os
 import argparse
 
+## quick options:
+# turn on random mesh coloring
+opt_random_mesh_color = 1
+
+
 ##{ main
 def main():
   # getting field name and type
@@ -466,6 +471,42 @@ def plot(regex,data_path):
     AnnotationAtts.axesArray.axes.tickMarks.majorSpacing = 0.2
     AnnotationAtts.axesArray.axes.grid = 0
     SetAnnotationAttributes(AnnotationAtts)
+    # set random color for coordinates lines
+    if (opt_random_mesh_color == 1):
+      MeshAtts = MeshAttributes()
+      MeshAtts.legendFlag = 1
+      MeshAtts.lineWidth = 0
+      MeshAtts.meshColor = (0, 0, 0, 255)
+      MeshAtts.meshColorSource = MeshAtts.MeshRandom  # Foreground, MeshCustom, MeshRandom
+      MeshAtts.opaqueColorSource = MeshAtts.Background  # Background, OpaqueCustom, OpaqueRandom
+      MeshAtts.opaqueMode = MeshAtts.Auto  # Auto, On, Off
+      MeshAtts.pointSize = 0.05
+      MeshAtts.opaqueColor = (255, 255, 255, 255)
+      MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+      MeshAtts.pointSizeVarEnabled = 0
+      MeshAtts.pointSizeVar = "default"
+      MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+      MeshAtts.showInternal = 0
+      MeshAtts.pointSizePixels = 2
+      MeshAtts.opacity = 1
+      SetPlotOptions(MeshAtts)
+      MeshAtts = MeshAttributes()
+      MeshAtts.legendFlag = 1
+      MeshAtts.lineWidth = 0
+      MeshAtts.meshColor = (0, 0, 0, 255)
+      MeshAtts.meshColorSource = MeshAtts.MeshRandom  # Foreground, MeshCustom, MeshRandom
+      MeshAtts.opaqueColorSource = MeshAtts.Background  # Background, OpaqueCustom, OpaqueRandom
+      MeshAtts.opaqueMode = MeshAtts.Auto  # Auto, On, Off
+      MeshAtts.pointSize = 0.05
+      MeshAtts.opaqueColor = (255, 255, 255, 255)
+      MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+      MeshAtts.pointSizeVarEnabled = 0
+      MeshAtts.pointSizeVar = "default"
+      MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+      MeshAtts.showInternal = 0
+      MeshAtts.pointSizePixels = 2
+      MeshAtts.opacity = 1
+      SetPlotOptions(MeshAtts)
 
   
 ##} plot
