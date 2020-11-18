2c9b5a8d9af4956645c85c7e50fa7ad39cb297e0
Alireza Rashti <rashti.alireza@gmail.com>
2020-11-18 13:54:53 -0500

    Adding a separate file for various functions used in Projects.

---
+++ Src/Main/Modules/Utilities/project_functions.h
@@ -0,0 +1,14 @@
+#include "core_lib.h"
+#include "manifold_lib.h"
+#include "utilities_lib.h"
+#include "maths_general_lib.h"
+#include "fields_lib.h"
+
+
+void 
+update_parameters_and_directories
+  (
+   const unsigned main_loop_iter,
+   const char *const dir_name_format/* eg: "BBN_%s_%ux%ux%u" */
+  );
+
