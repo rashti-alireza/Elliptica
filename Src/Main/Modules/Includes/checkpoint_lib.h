049ffdd643940d20c56f53ff2397f276e03ce8d3
Alireza Rashti <rashti.alireza@gmail.com>
2020-11-19 08:01:59 -0500

    Adding new module for checkpoint affairs.
    	new file:   checkpoint.c
    	new file:   checkpoint.h
    	new file:   ../Includes/checkpoint_lib.h

---
+++ Src/Main/Modules/Includes/checkpoint_lib.h
@@ -0,0 +1,14 @@
+#ifndef checkpoint_LIB_H
+#define checkpoint_LIB_H
+
+void write_checkpoint(Grid_T *const grid);
+void read_fields_from_checkpoint(Grid_T *const grid,FILE *const file);
+Grid_T *initi_from_checkpoint(FILE *const file);
+Parameter_T *parameter_query_from_checkpoint(const char *const par_name,FILE *const file);
+int is_checkpoint_sound(const char *const file_path);
+
+#endif
+
+
+
+
