906d4c54dc997283e147f9299c0047fbd50062f1
Alireza Rashti <rashti.alireza@gmail.com>
2020-11-18 22:34:29 -0500

    More entries for physics_compact_object_lib.h.

--- Src/Main/Modules/Includes/physics_compact_object_lib.h
+++ Src/Main/Modules/Includes/physics_compact_object_lib.h
@@ -1,29 +1,50 @@
 #ifndef physics_COMPACT_OBJ_LIB_H
 #define physics_COMPACT_OBJ_LIB_H
 
-/* commands */
+/* commands, DON'T change the numeration and ADD after one to the last */
 typedef enum CMD_T
 {
- CMD_UNDEFINED = -1,
- STRESS_ENERGY = 0,
- EULER_CONST   = 1,
- AH_RADIUS     = 2,
- P_ADM         = 3,
- AH_OMEGA      = 4,
- FORCE_BALANCE = 5,
- FIX_CENTER    = 6,
- FIND_SURFACE  = 7,
- AH_NORMAL_VECTOR    = 8,
- EXTRAPOLATE_OUTSIDE = 9,
+ CMD_UNDEFINED = 0,
+ STRESS_ENERGY = 1,
+ EULER_CONST   = 2,
+ AH_RADIUS     = 3,
+ P_ADM         = 4,
+ AH_OMEGA      = 5,
+ FORCE_BALANCE = 6,
+ FIX_CENTER    = 7,
+ FIND_SURFACE  = 8,
+ AH_NORMAL_VECTOR    = 9,
+ EXTRAPOLATE_OUTSIDE = 10,
  CMD_END
 }cmd_T;
+
+/* compact object type */
+typedef enum COMP_OBJ_T
+{
+ OBJ_UNDEFINED = 0,
+ NS,
+ NS1,
+ NS2,
+ BH,
+ BH1,
+ BH2,
+ BHNS
+ BHBH,
+ NSNS,
+}Com_Obj_T;
 
 /* struct for compact object */
 typedef struct COMPACT_OBJ_T
 {
  Grid_T *grid;
  Grid_Char_T *grid_char;/* grid character when used for surface finder */
- Flag_T pos;/* position of the object (LEFT|RIGHT|CENTER|NONE) */
+ Com_Obj_T type;/* BH1,NS2, NS, etc */
+ const char *stype;/* string of type (above) */
+ Com_Obj_T sys;/* system: BHNS, NSNS, BHBH, etc */
+ Flag_T pos;/* position of the object with respect to the whole grid
+            // i.e. (LEFT|RIGHT|CENTER|NONE) */
+ const char *spos;/* string type for position above, 
+                  // used for patch collections */
  
  
 }Compact_Obj_T;
@@ -31,6 +52,7 @@ typedef struct COMPACT_OBJ_T
 /* NOTE this MUST be as the same order as cmd_T */
 static const char *cmd_enum_str[] =
 {
+ "CMD_UNDEFINED",
  "STRESS_ENERGY",
  "EULER_CONST",
  "AH_RADIUS",
@@ -44,7 +66,7 @@ static const char *cmd_enum_str[] =
  0
 };
 
-Compact_Obj_T *init_compact_obj(Grid_T *const grid,const char *const obj);
+Compact_Obj_T *init_compact_obj(Grid_T *const grid,const char *const sobj);
 void free_compact_obj(Grid_T *const grid *obj);
 
 #endif
