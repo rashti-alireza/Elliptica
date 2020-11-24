62daa50f9f9b411cc46b151e3e68dbc263bf198f
Alireza Rashti <rashti.alireza@gmail.com>
2020-11-24 16:25:30 -0500

    Improve.

--- Src/Main/Modules/Physics/Star/star_NS.c
+++ Src/Main/Modules/Physics/Star/star_NS.c
@@ -78,9 +78,12 @@ int star_NS_idealfluid_extrapolate_matter_fields(Physics_T *const phys)
   {
     /* make phi, W => enthalpy */
     const char *fields_name[] = {"phi","enthalpy",0};
+    
     star_NS_extrapolate(phys,fields_name,"poly2");
+    
     phys->region = Ftype("NS_around");
     star_W_spin_vector_idealfluid_update(phys);
+    phys_set_region(phys);
   }
   else
   {
