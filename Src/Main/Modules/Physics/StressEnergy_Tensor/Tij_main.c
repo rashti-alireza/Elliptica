18990e66cf5db84606781bd713376577d745eeb8
Alireza Rashti <rashti.alireza@gmail.com>
2020-11-28 11:10:15 -0500

    Fix.

--- Src/Main/Modules/Physics/StressEnergy_Tensor/Tij_main.c
+++ Src/Main/Modules/Physics/StressEnergy_Tensor/Tij_main.c
@@ -12,64 +12,87 @@
 /* main function to issue command */
 int Tij_main(Physics_T *const phys)
 {
-  if (Pcmps("Tij_fluid","NS_ideal_fluid"))
+  switch (phys->cmd)
   {
-    if(Pcmps("Tij_NS_decomposition","CTS") &&
-       Pcmps("Tij_NS_gConf","non_flat"))
-    {
-      switch (phys->cmd)
-      {
-        case UPDATE_STRESS_ENERGY:
-          Tij_NS_idealfluid_CTS_gConf_update(phys);
-        break;
-        default:
-          Error0(NO_OPTION);
-      }
-    }
-    else
-    {
-      Error0(NO_OPTION);
-    }
-  }
-  else
-  {
-    Error0(NO_OPTION);
-  }
+    case UPDATE_STRESS_ENERGY:
+      Tij_update_stress_energy_tensor(phys);
+    break;
     
+    default:
+      Error0(NO_OPTION);
+  }
+  
   return EXIT_SUCCESS;
 }
 
-/* adding default parameters and fields. */
-int Tij_mount(Grid_T *const grid)
+
+/* add default parameters */
+int Tij_add_params(Physics_T *const phys)
 {
+  FUNC_TIC
+  
   /* fluid type:
+  // options:
   // NS_ideal_fluid: like: Phys. Rev. D 100, 124046  */
   Pset_default("Tij_fluid","NS_ideal_fluid");
   
   if (Pcmps("Tij_fluid","NS_ideal_fluid"))
   {
     /* decomposition type:
+    // options:
     // CTS (conformal thin sandwich): like: Phys. Rev. D 100, 124046  */
     Pset_default("Tij_NS_decomposition","CTS");
     
     /* conformal metric type: 
+    // options:
     // flat     => gConf  = delta_{ij},
     // non_flat => gConf != delta_{ij}. */
     Pset_default("Tij_NS_gConf","non_flat");
+  }
+  else
+    Error0(NO_OPTION);
     
-    if (Pcmps("Tij_NS_decomposition","CTS") &&
-        Pcmps("Tij_NS_gConf","non_flat"))
+  UNUSED(phys);
+  
+  FUNC_TOC
+  return EXIT_SUCCESS;
+}
+
+/* add fields parameters */
+int Tij_add_fields(Physics_T *const phys)
+{
+  FUNC_TIC
+  
+  if (Pcmps("Tij_NS_decomposition","CTS") &&
+      Pcmps("Tij_NS_gConf","non_flat"))
+  {
+    Tij_NS_idealfluid_CTS_gConf_add_fields(phys->grid);
+  }
+  else
+    Error0(NO_OPTION);
+    
+  FUNC_TOC
+  return EXIT_SUCCESS; 
+}
+
+/* update stress energy tensor */
+int Tij_update_stress_energy_tensor(Physics_T *const phys)
+{
+  FUNC_TIC
+  if (Pcmps("Tij_fluid","NS_ideal_fluid"))
+  {
+    if(Pcmps("Tij_NS_decomposition","CTS") &&
+       Pcmps("Tij_NS_gConf","non_flat"))
     {
-      Tij_NS_idealfluid_CTS_gConf_add_fields(grid);
+      Tij_NS_idealfluid_CTS_gConf_update(phys);
     }
     else
-    {
       Error0(NO_OPTION);
-    }
   }
   else
     Error0(NO_OPTION);
   
+  FUNC_TOC
   return EXIT_SUCCESS;
 }
 
--- Src/Main/Modules/Physics/StressEnergy_Tensor/Tij_main.h
+++ Src/Main/Modules/Physics/StressEnergy_Tensor/Tij_main.h
@@ -1,7 +1,8 @@
 #include "Tij_header.h"
 
 int Tij_main(Physics_T *const phys);
-int Tij_mount(Grid_T *const grid);
-
+int Tij_add_fields(Physics_T *const phys);
+int Tij_add_params(Physics_T *const phys);
+int Tij_update_stress_energy_tensor(Physics_T *const phys);
 
 
