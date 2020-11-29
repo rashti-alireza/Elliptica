9fd28b22486e579e0415b3f3128da0fb4ca5c8c1
Alireza Rashti <rashti.alireza@gmail.com>
2020-11-29 08:59:30 -0500

    Initiating free data physics.

---
+++ Src/Main/Modules/Physics/Free_Data/frd_header.h
@@ -0,0 +1,16 @@
+#ifndef frd_LIB_H
+#define frd_LIB_H
+
+#include "core_lib.h"
+#include "maths_general_lib.h"
+#include "maths_calculus_lib.h"
+#include "manifold_lib.h"
+#include "utilities_lib.h"
+#include "physics_lib.h"
+#include "fields_lib.h"
+
+int frd_main(Physics_T *const phys);
+
+
+#endif
+
---
+++ Src/Main/Modules/Physics/Free_Data/frd_main.h
@@ -0,0 +1,8 @@
+#include "frd_header.h"
+
+int frd_main(Physics_T *const phys);
+static int add_free_data_params(Physics_T *const phys);
+static int add_free_data_fields(Physics_T *const phys);
+
+
+
