0bf780b9f8c7ec1a995d900c9aaed9f78e01422a
Alireza Rashti <rashti.alireza@gmail.com>
2020-11-30 09:58:54 -0500

    Add Ricci computation.

--- Src/Main/Modules/Includes/maths_diff_geom_lib.h
+++ Src/Main/Modules/Includes/maths_diff_geom_lib.h
@@ -7,6 +7,11 @@ struct PATCH_T;
 
 void Christoffel_symbol_3d(struct PATCH_T *const patch,const char *const ig,const char *const dg,const char *const Chris);
 void dChristoffel_symbol_3d(Patch_T *const patch,const char *const dChris);
+void Ricci_3d(Patch_T *const patch,const char *const ig,
+              const char *const Chris,const char *const dChris,
+              const char *const Ricci,const char *const trRicci);
+
+
 
 #endif
 
--- Src/Main/Modules/Maths/Diff_Geom/dg_header.h
+++ Src/Main/Modules/Maths/Diff_Geom/dg_header.h
@@ -10,6 +10,9 @@
 void Christoffel_symbol_3d(Patch_T *const patch,const char *const ig,const char *const dg,const char *const Chris);
 void dChristoffel_symbol_3d(Patch_T *const patch,const char *const dChris);
 
+void Ricci_3d(Patch_T *const patch,const char *const ig,
+              const char *const Chris,const char *const dChris,
+              const char *const Ricci,const char *const trRicci);
 
 
 
