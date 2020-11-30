7857e366915b46a783565acbd7526ec27ced1f50
Alireza Rashti <rashti.alireza@gmail.com>
2020-11-30 07:51:50 -0500

    Add a module for differential geometry.

---
+++ Src/Main/Modules/Includes/maths_diff_geom_lib.h
@@ -0,0 +1,12 @@
+#ifndef maths_diff_geom_LIB_H
+#define maths_diff_geom_LIB_H
+
+/* forward declaration */
+
+struct PATCH_T;
+
+void Christoffel_symbol_3d(struct PATCH_T *const patch,const char *const ig,const char *const dg,const char *const Chris);
+
+
+#endif
+
---
+++ Src/Main/Modules/Maths/Diff_Geom/dg_header.h
@@ -0,0 +1,12 @@
+#include "core_lib.h"
+#include "error_handling_lib.h"
+#include "utilities_lib.h"
+#include "maths_general_lib.h"
+#include "maths_special_functions_lib.h"
+#include "manifold_lib.h"
+#include "fields_lib.h"
+
+void Christoffel_symbol_3d(Patch_T *const patch,const char *const ig,const char *const dg,const char *const Chris);
+
+
+
