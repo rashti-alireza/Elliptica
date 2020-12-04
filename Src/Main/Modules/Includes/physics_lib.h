3169f1bd4cfdb6e78882d81a68771a17d2b9103e
Alireza Rashti <rashti.alireza@gmail.com>
2020-12-04 16:40:28 -0500

    Spruce up.

--- Src/Main/Modules/Includes/physics_lib.h
+++ Src/Main/Modules/Includes/physics_lib.h
@@ -124,7 +124,8 @@
 
 /* rest assure the criterion is met for physics type
 // usage ex: AssureType(phys->ctype == NS). */
-#define AssureType(x) {if (!(x)) Error1("Wrong physics passed to %s\n",__func__);}
+#define AssureType(x) \
+ {if (!(x)) Error1("Wrong physics passed to function '%s'\n",__func__);}
 
 /* commands, DON'T change the numeration. */
 typedef enum CMD_T
