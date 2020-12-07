4004334b8c2fba257868ef68234bd53416c38334
Alireza Rashti <rashti.alireza@gmail.com>
2020-12-07 07:49:23 -0500

    Lower polynomial degree.

--- Src/Main/Modules/Physics/BlackHole/bh_bhf_ChebTn_filler.c
+++ Src/Main/Modules/Physics/BlackHole/bh_bhf_ChebTn_filler.c
@@ -7,36 +7,24 @@ void bh_bhf_ChebTn_extrapolate
 void bh_bhf_ChebTn_extrapolate
 (double *const a,const double fr0,const double fr1,const double dfdr,const double ddfddr,const double rfill,const Uint N)
 {
-assert(N==10);
+assert(N==6);
 a[0] = 
-(311.0/16384.0)*ddfddr*pow(rfill, 2) - 7809.0/65536.0*dfdr*rfill + 
-(14179.0/32768.0)*fr0 + (18589.0/32768.0)*fr1;
+(3.0/128.0)*ddfddr*pow(rfill, 2) - 37.0/256.0*dfdr*rfill + (49.0/
+128.0)*fr0 + (79.0/128.0)*fr1;
 a[1] = 
-(69.0/32768.0)*ddfddr*pow(rfill, 2) - 2971.0/65536.0*dfdr*rfill - 
-4851.0/8192.0*fr0 + (4851.0/8192.0)*fr1;
+-1.0/256.0*ddfddr*pow(rfill, 2) - 5.0/256.0*dfdr*rfill - 35.0/64.0*
+fr0 + (35.0/64.0)*fr1;
 a[2] = 
--117.0/4096.0*ddfddr*pow(rfill, 2) + (1225.0/8192.0)*dfdr*rfill + 
-(441.0/4096.0)*fr0 - 441.0/4096.0*fr1;
+-1.0/32.0*ddfddr*pow(rfill, 2) + (11.0/64.0)*dfdr*rfill + (5.0/32.0)*
+fr0 - 5.0/32.0*fr1;
 a[3] = 
--49.0/16384.0*ddfddr*pow(rfill, 2) + (1715.0/32768.0)*dfdr*rfill + 
-(441.0/4096.0)*fr0 - 441.0/4096.0*fr1;
+(3.0/512.0)*ddfddr*pow(rfill, 2) + (7.0/512.0)*dfdr*rfill + (5.0/
+128.0)*fr0 - 5.0/128.0*fr1;
 a[4] = 
-(49.0/4096.0)*ddfddr*pow(rfill, 2) - 637.0/16384.0*dfdr*rfill - 441.0/
-8192.0*fr0 + (441.0/8192.0)*fr1;
+(1.0/128.0)*ddfddr*pow(rfill, 2) - 7.0/256.0*dfdr*rfill - 5.0/128.0*
+fr0 + (5.0/128.0)*fr1;
 a[5] = 
-(11.0/16384.0)*ddfddr*pow(rfill, 2) - 217.0/32768.0*dfdr*rfill - 63.0/
-4096.0*fr0 + (63.0/4096.0)*fr1;
-a[6] = 
--11.0/4096.0*ddfddr*pow(rfill, 2) + (79.0/8192.0)*dfdr*rfill + (63.0/
-4096.0)*fr0 - 63.0/4096.0*fr1;
-a[7] = 
-(19.0/65536.0)*ddfddr*pow(rfill, 2) - 85.0/131072.0*dfdr*rfill - 9.0/
-16384.0*fr0 + (9.0/16384.0)*fr1;
-a[8] = 
-(5.0/16384.0)*ddfddr*pow(rfill, 2) - 75.0/65536.0*dfdr*rfill - 63.0/
-32768.0*fr0 + (63.0/32768.0)*fr1;
-a[9] = 
--5.0/65536.0*ddfddr*pow(rfill, 2) + (35.0/131072.0)*dfdr*rfill + (7.0/
-16384.0)*fr0 - 7.0/16384.0*fr1;
+-1.0/512.0*ddfddr*pow(rfill, 2) + (3.0/512.0)*dfdr*rfill + (1.0/128.0)*
+fr0 - 1.0/128.0*fr1;
 }
 
