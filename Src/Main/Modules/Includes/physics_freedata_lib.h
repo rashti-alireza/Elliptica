131bbfc466f90f2ca397b1e0481e23131fcc9e80
Alireza Rashti <rashti.alireza@gmail.com>
2020-12-10 11:55:21 -0500

    Update physics_freedata_lib header.

--- Src/Main/Modules/Includes/physics_freedata_lib.h
+++ Src/Main/Modules/Includes/physics_freedata_lib.h
@@ -18,6 +18,38 @@ fd_populate_psi_alphaPsi_beta_KerrSchild
  const char *const ig/*(inverse metric) if ig is null, it makes them */
  );
 
+void 
+fd_populate_gConf_dgConf_igConf_KerrSchild
+ (
+ Physics_T *const phys,
+ const char *const region/* where computations take place */,
+ const char *const gConf/* metric stem */,
+ const char *const igConf/* inverse of metric stem */,
+ const char *const dgConf/* derivative of metric stem */
+ );
+
+void fd_1st_derivative_Christoffel_symbol(Physics_T *const phys,
+                                            const char *const region,
+                                            const char *const dChris);
+
+void fd_conformal_Ricci(Physics_T *const phys,
+                          const char *const region,
+                          const char *const ig,
+                          const char *const Chris,
+                          const char *const dChris,
+                          const char *const RicciConf,
+                          const char *const trRicciConf);
+
+void fd_extrinsic_curvature_KerrSchild(Physics_T *const phys,
+                                         const char *const region,
+                                         const char *const ig,
+                                         const char *const Chris,
+                                         const char *const Kij,
+                                         const char *const trK,
+                                         const char *const dtrK);
+
+
+
 
 #endif
 
