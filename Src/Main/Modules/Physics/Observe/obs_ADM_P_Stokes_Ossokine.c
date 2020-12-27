a836565294bef1eff5868eef3bc867049cd5c695
Alireza Rashti <rashti.alireza@gmail.com>
2020-12-27 11:25:00 -0500

    Improve.

--- Src/Main/Modules/Physics/Observe/obs_ADM_P_Stokes_Ossokine.c
+++ Src/Main/Modules/Physics/Observe/obs_ADM_P_Stokes_Ossokine.c
@@ -4,13 +4,6 @@
 */
 
 
-#include "core_lib.h"
-#include "physics_observe_lib.h"
-#include "utilities_lib.h"
-#include "manifold_lib.h"
-#include "maths_general_lib.h"
-#include "maths_calculus_lib.h"
-#include "fields_lib.h"
 #include "obs_header.h"
 
 
