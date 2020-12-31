9fad4a2020a1e93feceb9d4db3ee93d3ee392d2c
Alireza Rashti <rashti.alireza@gmail.com>
2020-12-31 12:43:34 -0500

    Add macros to set and read parameter for each object in equations.

--- Src/Main/Modules/Physics/Equation/eq_header.h
+++ Src/Main/Modules/Physics/Equation/eq_header.h
@@ -12,13 +12,51 @@
 #include "maths_equation_solvings_lib.h"
 #include "maths_spectral_methods_lib.h"
 
+/* string leng */
+#define EQ__STR__LEN0 (33)
+#define EQ__STR__LEN1 (99)
+
 /* prefix of internal parameters of this project */
 #define P_  "EQ_"
 
 /* backup field name prefix */
 #define P_Backup_  P_"backup_"
 
+/* define paramter and prefix used in EQ_Set_Prefix and EQ_PrefixIt*/
+#define EQ_Def_Param_Prefix_Char \
+char EQ__param__prefix[EQ__STR__LEN0] = {'\0'}; \
+char EQ__param[EQ__STR__LEN1] = {'\0'};\
+char EQ__Temp1[EQ__STR__LEN0] = {'\0'};\
+char EQ__Temp2[EQ__STR__LEN0] = {'\0'};
 
+/* define paramter prefix for given object X,
+// this will be used by EQ_PrefixIt macro.
+// the purpose of this macro is to find the correct
+// index when multiple objects involved and 
+// there are some paramters for each object for instance BHBH
+// system which has BH1 and BH2, in this case, for example,
+// if X is "BH" and patch->name is "grid1_BH2_around_IB_X0Y0Z0"
+// EQ__param__prefix will be "BH2" according to the patch->name.
+// if given X has already index then EQ__param__prefix = X
+// this is working because while setting up the region on each equation 
+// only pertinent patches have been chosen. */
+#define EQ_Set_Prefix(X) \
+sprintf(EQ__temp1,"%s1",X);\
+sprintf(EQ__temp2,"%s2",X);\
+if (strchr(X,'1') || strchr(X,'2'))\
+  sprintf(EQ__param__prefix,"%s",X);\
+else if (strstr(patch->name,EQ__temp1))\
+  sprintf(EQ__param__prefix,"%s1",X);\
+else if (strstr(patch->name,EQ__temp2))\
+  sprintf(EQ__param__prefix,"%s2",X);\
+else\
+  sprintf(EQ__param__prefix,"%s",X);
+
+/* prefix given parameter X by EQ_prefix_par defined in 
+// EQ_Set_Prefix. */
+#define EQ_PrefixIt(X) \
+  (sprintf(EQ__param,"%s_%s",EQ__param_prefix,X) ? EQ__param : NULL)
+  
 void eq_solve_elliptic_equation(Physics_T *const phys);
 
 
