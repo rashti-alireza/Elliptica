5ac3a2c3dfdcf8a3f49b1211a5ca479a0a8c766d
Alireza Rashti <rashti.alireza@gmail.com>
2021-01-01 10:32:10 -0500

    Fix function prototype in header.

--- Src/Main/Modules/Physics/Equation/eq_db_header.h
+++ Src/Main/Modules/Physics/Equation/eq_db_header.h
@@ -1,39 +1,39 @@
 /* equations data base header */
 
-void eq_XCTS_curve_Wolf_ddm_eq_phi(void *vp1,void *vp2);
-void eq_XCTS_curve_Wolf_ddm_bc_phi(void *vp1,void *vp2);
-void eq_XCTS_curve_Wolf_ddm_jacobian_eq_phi(void *vp1,void *vp2);
-void eq_XCTS_curve_Wolf_ddm_jacobian_bc_phi(void *vp1,void *vp2);
+void *eq_XCTS_curve_Wolf_ddm_eq_phi(void *vp1,void *vp2);
+void *eq_XCTS_curve_Wolf_ddm_bc_phi(void *vp1,void *vp2);
+void *eq_XCTS_curve_Wolf_ddm_jacobian_eq_phi(void *vp1,void *vp2);
+void *eq_XCTS_curve_Wolf_ddm_jacobian_bc_phi(void *vp1,void *vp2);
 
 
-void eq_XCTS_curve_exc_KS_ddm_eq_psi(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_bc_psi(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_eq_psi(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_bc_psi(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_eq_psi(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_bc_psi(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_eq_psi(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_bc_psi(void *vp1,void *vp2);
 
 
-void eq_XCTS_curve_exc_KS_ddm_eq_alphaPsi(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_bc_alphaPsi(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_eq_alphaPsi(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_bc_alphaPsi(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_eq_alphaPsi(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_bc_alphaPsi(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_eq_alphaPsi(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_bc_alphaPsi(void *vp1,void *vp2);
 
 
-void eq_XCTS_curve_exc_KS_ddm_eq_B0_U0(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_bc_B0_U0(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_eq_B0_U0(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_bc_B0_U0(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_eq_B0_U0(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_bc_B0_U0(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_eq_B0_U0(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_bc_B0_U0(void *vp1,void *vp2);
 
 
-void eq_XCTS_curve_exc_KS_ddm_eq_B0_U1(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_bc_B0_U1(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_eq_B0_U1(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_bc_B0_U1(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_eq_B0_U1(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_bc_B0_U1(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_eq_B0_U1(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_bc_B0_U1(void *vp1,void *vp2);
 
 
-void eq_XCTS_curve_exc_KS_ddm_eq_B0_U2(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_bc_B0_U2(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_eq_B0_U2(void *vp1,void *vp2);
-void eq_XCTS_curve_exc_KS_ddm_jacobian_bc_B0_U2(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_eq_B0_U2(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_bc_B0_U2(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_eq_B0_U2(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_KS_ddm_jacobian_bc_B0_U2(void *vp1,void *vp2);
 
 
 
