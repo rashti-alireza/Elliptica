50f76c35ca5c1ad66680aff4ea8357b7f5afe56d
Alireza Rashti <rashti.alireza@gmail.com>
2021-01-28 08:04:07 -0500

    Add new option for matter extrapolation.

--- Src/Main/Modules/Physics/Star/star_NS.c
+++ Src/Main/Modules/Physics/Star/star_NS.c
@@ -284,7 +284,16 @@ int star_NS_idealfluid_extrapolate_matter_fields(Physics_T *const phys)
 {
   FUNC_TIC
   
-  IF_sval("extrapolate_matter_fields","inverse_r2_expmAr")
+  IF_sval("extrapolate_matter_fields","inverse_r_expmAr")
+  {
+    /* make phi, W => enthalpy */
+    const char *fields_name[] = {"phi","enthalpy",0};
+    
+    star_NS_extrapolate(phys,fields_name,"inverse_r_expmAr");
+    
+    star_W_spin_vector_idealfluid_update(phys,"NS_around");
+  }
+  else IF_sval("extrapolate_matter_fields","inverse_r2_expmAr")
   {
     /* make phi, W => enthalpy */
     const char *fields_name[] = {"phi","enthalpy",0};
