ac535ab5a448f276ba8a22683fe07a6c04ae1353
Alireza Rashti <rashti.alireza@gmail.com>
2021-02-23 10:17:35 -0500

    Define inline keywords.

--- Src/Main/Core/elliptica_system_lib.h
+++ Src/Main/Core/elliptica_system_lib.h
@@ -1,7 +1,15 @@
 #ifndef elliptica_system_LIB_H
 #define elliptica_system_LIB_H
 
+/* define handy macro for unsigned */
 #define Uint unsigned
+
+/* define inline calls */
+#ifdef INLINE_FUNC
+#define INLINE inline
+#else
+#define INLINE 
+#endif
 
 #endif
 
