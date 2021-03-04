408ccb0b47cf0f7964a02b585791c627140f52c0
Alireza Rashti <rashti.alireza@gmail.com>
2021-03-04 12:56:31 -0500

    Better error msg.

--- Src/Main/Modules/Utilities/useful_functions.c
+++ Src/Main/Modules/Utilities/useful_functions.c
@@ -881,8 +881,8 @@ void interpolate_fields_from_old_grid_to_new_grid
           opatch = x_in_which_patch_force
             (patch->node[ijk]->x,ogrid->patch,ogrid->np,pnts[p][ijk].X);
           if(!opatch)
-            Error0("Could not find a patch!"
-                   "(probably too low resolution).");
+            Errors("I could not find a patch for %s.\nProbably too "
+                   "low resolution or strange configuration.",patch->name);
           pnts[p][ijk].patch = opatch;
           continue;
         }
