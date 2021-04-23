520a4dbf2c2ee3cacc42df57ece35ce2574c3d44
Alireza Rashti <rashti.alireza@gmail.com>
2021-04-23 11:45:37 -0400

    Add b.c. for alpha equation. Mostly employed when using conformal flat metric.

--- Src/Main/Modules/Physics/Equation/eq_db_header.h
+++ Src/Main/Modules/Physics/Equation/eq_db_header.h
@@ -16,7 +16,8 @@ void *eq_XCTS_curve_exc_KS_ddm_eq_alphaPsi(void *vp1,void *vp2);
 void *eq_XCTS_curve_exc_KS_ddm_bc_alphaPsi(void *vp1,void *vp2);
 void *eq_XCTS_curve_exc_KS_ddm_jacobian_eq_alphaPsi(void *vp1,void *vp2);
 void *eq_XCTS_curve_exc_KS_ddm_jacobian_bc_alphaPsi(void *vp1,void *vp2);
-
+void *eq_XCTS_curve_exc_Nmnn_ddm_bc_alphaPsi(void *vp1,void *vp2);
+void *eq_XCTS_curve_exc_Nmnn_ddm_jacobian_bc_alphaPsi(void *vp1,void *vp2);
 
 void *eq_XCTS_curve_exc_KS_ddm_eq_B0_U0(void *vp1,void *vp2);
 void *eq_XCTS_curve_exc_KS_ddm_bc_B0_U0(void *vp1,void *vp2);
