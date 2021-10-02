4134f0ba00a941a2893af3c8a21e8f3bf9571134
Alireza Rashti <rashti.alireza@gmail.com>
2021-10-02 10:36:05 -0400

    Turn on E_Trans_prime_IS_CCS_FORMAT.

--- Src/Main/Modules/Maths/Equation_Solvings/solve_eqs_ddm_schur_complement.h
+++ Src/Main/Modules/Maths/Equation_Solvings/solve_eqs_ddm_schur_complement.h
@@ -28,8 +28,8 @@
 
 /* NOTE: E_Trans_prime_IS_RMO_FORMAT and E_Trans_prime_IS_CCS_FORMAT 
 // MUST be mutually exclusive. */
-#define E_Trans_prime_IS_RMO_FORMAT (1)
-#define E_Trans_prime_IS_CCS_FORMAT (0)
+#define E_Trans_prime_IS_RMO_FORMAT (0)
+#define E_Trans_prime_IS_CCS_FORMAT (1)
 
 /* string size */
 #define MSG_SIZE1 (999)
