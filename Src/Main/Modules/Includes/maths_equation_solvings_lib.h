5b6d8ad5caf881d43a566674ff93e5e6ed2eae2f
Alireza Rashti <rashti.alireza@gmail.com>
2021-12-01 17:55:46 -0500

    Use a compact notation for setting temp vars if jacobian_workspace.

--- Src/Main/Modules/Includes/maths_equation_solvings_lib.h
+++ Src/Main/Modules/Includes/maths_equation_solvings_lib.h
@@ -6,11 +6,13 @@
 
 #define MAX_STR_MATH_EQ_SOLVE_LIB (400)
 
+/* a short hand notation for: */
+#define J__JW  (patch->solving_man->jacobian_workspace)
+
 /* NOTE: SPECTRAL_JACOBIAN_MATRIX_FORM and 
 // SPECTRAL_JACOBIAN_ANALYTIC_FORM must be mutually exclusive. */
 #define SPECTRAL_JACOBIAN_MATRIX_FORM (0)
 #define SPECTRAL_JACOBIAN_ANALYTIC_FORM (1)
-
 
 #if SPECTRAL_JACOBIAN_MATRIX_FORM
 
@@ -35,6 +37,10 @@
 #define d3f_dx2du_Jacobian(patch,dx_axis,ijk,lmn,xNAME) \
   ( f_##xNAME(m_##xNAME, ijk, lmn) )
 
+#define J__set_temp_vars_JW_ijk(Xijk) /* nothing! */
+
+#define J__set_temp_vars_JW_lmn(Xlmn) /* nothing! */
+
 
 #elif SPECTRAL_JACOBIAN_ANALYTIC_FORM
 
@@ -55,7 +61,21 @@
 #define d3f_dx2du_Jacobian(patch,dxdy_axis,ijk,lmn,xNAME) \
   d3f_dxdydu_optimized_spectral_Jacobian_analytic(patch,dxdy_axis)
 //  d3f_dxdydu_spectral_Jacobian_analytic(patch,dxdy_axis,ijk,lmn)
-  
+
+/* setting temporary vars */ 
+#define J__set_temp_vars_JW_ijk(Xijk) \
+    J__JW->ijk = (Xijk);\
+    ijk_to_i_j_k((Xijk),patch->n,&(J__JW->i),&(J__JW->j),&(J__JW->k));\
+    J__JW->sin_thi[0] = sin(J__JW->i*J__JW->pi_o_nm1[0]);\
+    J__JW->sin_thi[1] = sin(J__JW->j*J__JW->pi_o_nm1[1]);\
+    J__JW->sin_thi[2] = sin(J__JW->k*J__JW->pi_o_nm1[2]);\
+    J__JW->cos_thi[0] = cos(J__JW->i*J__JW->pi_o_nm1[0]);\
+    J__JW->cos_thi[1] = cos(J__JW->j*J__JW->pi_o_nm1[1]);\
+    J__JW->cos_thi[2] = cos(J__JW->k*J__JW->pi_o_nm1[2]);
+
+#define J__set_temp_vars_JW_lmn(Xlmn) \
+    J__JW->lmn = (Xlmn);\
+    ijk_to_i_j_k((Xlmn),patch->n,&(J__JW->l),&(J__JW->m),&(J__JW->n));
 
 #endif
 
@@ -75,27 +95,17 @@
   const Uint Nk = patch->nn;/* total number of nodes */\
   Uint i,j,k;
 
-/* a short hand notation for: */
-#define J__JW  (patch->solving_man->jacobian_workspace)
 
 /* macro for B part of jacobian */
 #define DDM_SCHUR_JACOBIAN_EQ_Bpart_OPEN \
   for (i = 0; i < Ni; ++i)\
   {\
     ijk = node[i];\
-    J__JW->ijk = ijk;\
-    ijk_to_i_j_k(ijk,patch->n,&(J__JW->i),&(J__JW->j),&(J__JW->k));\
-    J__JW->sin_thi[0] = sin(J__JW->i*J__JW->pi_o_nm1[0]);\
-    J__JW->sin_thi[1] = sin(J__JW->j*J__JW->pi_o_nm1[1]);\
-    J__JW->sin_thi[2] = sin(J__JW->k*J__JW->pi_o_nm1[2]);\
-    J__JW->cos_thi[0] = cos(J__JW->i*J__JW->pi_o_nm1[0]);\
-    J__JW->cos_thi[1] = cos(J__JW->j*J__JW->pi_o_nm1[1]);\
-    J__JW->cos_thi[2] = cos(J__JW->k*J__JW->pi_o_nm1[2]);\
+    J__set_temp_vars_JW_ijk(ijk)\
     for (j = 0; j < Nj; ++j)\
     {\
       lmn = node[j];\
-      J__JW->lmn = lmn;\
-      ijk_to_i_j_k(lmn,patch->n,&(J__JW->l),&(J__JW->m),&(J__JW->n));
+      J__set_temp_vars_JW_lmn(lmn)
 
 #define DDM_SCHUR_JACOBIAN_EQ_Bpart_CLOSE \
     }/* end of for (i = 0; i < Ni; ++i) */\
@@ -109,20 +119,12 @@
     for (i = 0; i < Ni; ++i)\
     {\
       ijk = node[i];\
-      J__JW->ijk = ijk;\
-      ijk_to_i_j_k(ijk,patch->n,&(J__JW->i),&(J__JW->j),&(J__JW->k));\
-      J__JW->sin_thi[0] = sin(J__JW->i*J__JW->pi_o_nm1[0]);\
-      J__JW->sin_thi[1] = sin(J__JW->j*J__JW->pi_o_nm1[1]);\
-      J__JW->sin_thi[2] = sin(J__JW->k*J__JW->pi_o_nm1[2]);\
-      J__JW->cos_thi[0] = cos(J__JW->i*J__JW->pi_o_nm1[0]);\
-      J__JW->cos_thi[1] = cos(J__JW->j*J__JW->pi_o_nm1[1]);\
-      J__JW->cos_thi[2] = cos(J__JW->k*J__JW->pi_o_nm1[2]);\
+      J__set_temp_vars_JW_ijk(ijk)\
       for (k = K0; k < Nk; ++k)\
       {\
         lmn = node[k];\
         j = k-K0;\
-        J__JW->lmn = lmn;\
-        ijk_to_i_j_k(lmn,patch->n,&(J__JW->l),&(J__JW->m),&(J__JW->n));
+        J__set_temp_vars_JW_lmn(lmn)
 
         
 #define DDM_SCHUR_JACOBIAN_EQ_Epart_CLOSE \
@@ -150,19 +152,11 @@
   for (i = I0; i < Ni; ++i)\
   {\
     ijk = node[i];\
-    J__JW->ijk = ijk;\
-    ijk_to_i_j_k(ijk,patch->n,&(J__JW->i),&(J__JW->j),&(J__JW->k));\
-    J__JW->sin_thi[0] = sin(J__JW->i*J__JW->pi_o_nm1[0]);\
-    J__JW->sin_thi[1] = sin(J__JW->j*J__JW->pi_o_nm1[1]);\
-    J__JW->sin_thi[2] = sin(J__JW->k*J__JW->pi_o_nm1[2]);\
-    J__JW->cos_thi[0] = cos(J__JW->i*J__JW->pi_o_nm1[0]);\
-    J__JW->cos_thi[1] = cos(J__JW->j*J__JW->pi_o_nm1[1]);\
-    J__JW->cos_thi[2] = cos(J__JW->k*J__JW->pi_o_nm1[2]);\
+    J__set_temp_vars_JW_ijk(ijk)\
     for (j = 0; j < Nj; ++j)\
     {\
       lmn = node[j];\
-      J__JW->lmn = lmn;\
-      ijk_to_i_j_k(lmn,patch->n,&(J__JW->l),&(J__JW->m),&(J__JW->n));
+      J__set_temp_vars_JW_lmn(lmn)
 
 
 #define DDM_SCHUR_JACOBIAN_BC_Bpart_CLOSE \
@@ -177,20 +171,12 @@
     for (i = I0; i < Ni; ++i)\
     {\
       ijk = node[i];\
-      J__JW->ijk = ijk;\
-      ijk_to_i_j_k(ijk,patch->n,&(J__JW->i),&(J__JW->j),&(J__JW->k));\
-      J__JW->sin_thi[0] = sin(J__JW->i*J__JW->pi_o_nm1[0]);\
-      J__JW->sin_thi[1] = sin(J__JW->j*J__JW->pi_o_nm1[1]);\
-      J__JW->sin_thi[2] = sin(J__JW->k*J__JW->pi_o_nm1[2]);\
-      J__JW->cos_thi[0] = cos(J__JW->i*J__JW->pi_o_nm1[0]);\
-      J__JW->cos_thi[1] = cos(J__JW->j*J__JW->pi_o_nm1[1]);\
-      J__JW->cos_thi[2] = cos(J__JW->k*J__JW->pi_o_nm1[2]);\
+      J__set_temp_vars_JW_ijk(ijk)\
       for (k = K0; k < Nk; ++k)\
       {\
         lmn = node[k];\
         j = k-K0;\
-        J__JW->lmn = lmn;\
-        ijk_to_i_j_k(lmn,patch->n,&(J__JW->l),&(J__JW->m),&(J__JW->n));
+        J__set_temp_vars_JW_lmn(lmn)
 
 #define DDM_SCHUR_JACOBIAN_BC_Epart_CLOSE \
      }/* end of for (i = I0; i < Ni; ++i) */\
