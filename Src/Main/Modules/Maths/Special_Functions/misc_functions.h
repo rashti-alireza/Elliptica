3d1714d98981554fc9e0b172487709ac56c28799
Alireza Rashti <rashti.alireza@gmail.com>
2021-12-01 14:13:27 -0500

    Update header lib for dT/dx.

--- Src/Main/Modules/Maths/Special_Functions/misc_functions.h
+++ Src/Main/Modules/Maths/Special_Functions/misc_functions.h
@@ -4,4 +4,7 @@ int Factorial(const int n);
 double Cheb_Un(const int n, const double x);
 double Cheb_Tn(const int n, const double x);
 double d2T_dx2(const int n, const double x);
+double dT_dx(const int n,const double x);
+
+
 
