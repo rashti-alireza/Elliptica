851a1aa6e2b6b4f172b0da5fea62b75fb750f2bb
Alireza Rashti <rashti.alireza@gmail.com>
2022-07-17 16:45:19 -0400

    Add a bash file for postprocessing utiliteies.

---
+++ Scripts/postprocess_utils.sh
@@ -0,0 +1,27 @@
+# Alireza Rashti July 2022
+#
+# variety of utilities for postprocessing to be used by other scripts
+#
+
+
+## find the field position $1 on the header of given file $2
+## using echo, it returns the column number starting from 0
+function find_field_position_header {
+	local counter=0
+	local header=$(head -n1 $2)
+	local ret=""
+	
+	for i in $header
+	do
+		if [[ "$1" == "$i" ]]
+		then
+			ret=$counter
+		fi
+		counter=$((counter+1))
+	done
+	echo -n "${ret}"
+	
+	return 0
+}
+
+#export -f find_field_position_header
