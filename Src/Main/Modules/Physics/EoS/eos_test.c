8ed01439784754c53ee2fb5cf0f609f0efb339a5
Alireza <rashti.alireza@gmail.com>
2022-10-16 09:38:30 -0400

    Adding specific_internal_energy. 	Changing the output test format.

--- Src/Main/Modules/Physics/EoS/eos_test.c
+++ Src/Main/Modules/Physics/EoS/eos_test.c
@@ -28,60 +28,70 @@ void test_EoS(Physics_T *const phys)
     sprintf(file_name,"%s/%s.pwp",path,eos->description);
     file = Fopen(file_name,"w+");
     
-    fprintf(file,"piece  Kappa         rho           gamma         a             h-1\n");
+    fprintf(file,"1:piece  2:Kappa  3:rho  4:gamma  5:a  6:h-1\n");
     for (i = 0; i < eos->N; ++i)  
-      fprintf(file,"%u      %e  %e  %e  %e  %e\n",i,eos->K[i],eos->rho0_th[i],eos->gamma[i],eos->a[i],eos->h_th[i]-1);
+      fprintf(file,"%u %0.15f %0.15f %0.15f %0.15f %0.15f\n",i,eos->K[i],eos->rho0_th[i],eos->gamma[i],eos->a[i],eos->h_th[i]-1);
     Fclose(file);
   }
     
   /* continuity */
   sprintf(file_name,"%s/%s",path,"pressure");
   file = Fopen(file_name,"w+");
-  fprintf(file,"# enthalpy   pressure\n");  
+  fprintf(file,"# enthalpy  pressure\n");  
   for (i = 0; i < N; ++i)
   {
     eos->h = 1+s*i;
-    fprintf(file,"  %-7g    %-7g\n",eos->h,eos->pressure(eos));
+    fprintf(file,"%0.15f %0.15f\n",eos->h,eos->pressure(eos));
   }
   Fclose(file);
   
   sprintf(file_name,"%s/%s",path,"rest_mass_density");
   file = Fopen(file_name,"w+");
-  fprintf(file,"# enthalpy   rest_mass_density\n");  
+  fprintf(file,"# enthalpy  rest_mass_density\n");  
   for (i = 0; i < N; ++i)
   {
     eos->h = 1+s*i;
-    fprintf(file,"  %-7g    %-7g\n",eos->h,eos->rest_mass_density(eos));
+    fprintf(file,"%0.15f %0.15f\n",eos->h,eos->rest_mass_density(eos));
   }
   Fclose(file);
   
   sprintf(file_name,"%s/%s",path,"energy_density");
   file = Fopen(file_name,"w+");
-  fprintf(file,"# enthalpy   energy_density\n");
+  fprintf(file,"# enthalpy  energy_density\n");
   for (i = 0; i < N; ++i)
   {
     eos->h = 1+s*i;
-    fprintf(file,"  %-7g    %-7g\n",eos->h,eos->energy_density(eos));
+    fprintf(file,"%0.15f %0.15f\n",eos->h,eos->energy_density(eos));
+  }
+  Fclose(file);
+  
+  sprintf(file_name,"%s/%s",path,"specific_internal_energy");
+  file = Fopen(file_name,"w+");
+  fprintf(file,"# enthalpy  specific_internal_energy\n");
+  for (i = 0; i < N; ++i)
+  {
+    eos->h = 1+s*i;
+    fprintf(file,"%0.15f %0.15f\n",eos->h,eos->specific_internal_energy(eos));
   }
   Fclose(file);
   
   sprintf(file_name,"%s/%s",path,"drho0_dh");
   file = Fopen(file_name,"w+");
-  fprintf(file,"# enthalpy   drho0_dh\n");  
+  fprintf(file,"# enthalpy  drho0_dh\n");  
   for (i = 0; i < N; ++i)
   {
     eos->h = 1+s*i;
-    fprintf(file,"  %-7g    %-7g\n",eos->h,eos->drho0_dh(eos));
+    fprintf(file,"%0.15f %0.15f\n",eos->h,eos->drho0_dh(eos));
   }
   Fclose(file);
   
   sprintf(file_name,"%s/%s",path,"de_dh");
   file = Fopen(file_name,"w+");
-  fprintf(file,"# enthalpy   de_dh\n");  
+  fprintf(file,"# enthalpy  de_dh\n");  
   for (i = 0; i < N; ++i)
   {
     eos->h = 1+s*i;
-    fprintf(file,"  %-7g    %-7g\n",eos->h,eos->de_dh(eos));
+    fprintf(file,"%0.15f %0.15f\n",eos->h,eos->de_dh(eos));
   }
   Fclose(file);
   
