c302eae9fa951dbada45920a410f2885ec0eb2bf
Alireza <numerical.relativity@gmail.com>
2023-02-05 12:32:36 -0500

    Added params and ifield entry.

--- Src/Projects/Initial_Data_Reader/elliptica_id_reader_lib.h
+++ Src/Projects/Initial_Data_Reader/elliptica_id_reader_lib.h
@@ -7,6 +7,7 @@
 typedef struct ELLIPTICA_ID_READER_T
 {
   char checkpoint_path[EIDR_MAX_STR];// path/to/elliptica/checkpoint/file
+  const char *ifields;// input fields, e.g., "alpha,betax,adm_gxx"
   char *system;// the ID system, e.g., BHNS, NSNS, etc.
   Uint nfield;// total number of Field_Dictionary
   double **field;// value = field[indx(Field_Dictionary[i])][ijk]
@@ -15,6 +16,9 @@ typedef struct ELLIPTICA_ID_READER_T
   double *y_coords;// Cartesian y coord values, y_coords[ijk]
   double *z_coords;// Cartesian z coord values, z_coords[ijk]
   Uint (*indx)(const char *const fname);// find field index  
+  char **params_lv;// left value,  params[0]="force_balance"
+  char **params_rv;// right value, params[0]="on"
+  Uint nparams;// number of params
 }Elliptica_ID_Reader_T;
 
 #undef EIDR_MAX_STR
