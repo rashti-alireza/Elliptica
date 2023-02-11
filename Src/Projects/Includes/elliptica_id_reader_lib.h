dfef1a9e3a77d40293867212825531b2f30a3c99
Alireza <numerical.relativity@gmail.com>
2023-02-10 21:19:29 -0500

    Comments. Used unsigned instead of Uint.

--- Src/Projects/Includes/elliptica_id_reader_lib.h
+++ Src/Projects/Includes/elliptica_id_reader_lib.h
@@ -1,7 +1,8 @@
 #ifndef ELLIPTICA_ID_READER_LIB_H
 #define ELLIPTICA_ID_READER_LIB_H
 
-#include "elliptica_system_lib.h"
+/* NOTE: this is the header to be used for an evolution code */
+/* NOTE: unsigned is not used here in case there is any conflicts for evo codes. */
 
 #define EIDR_MAX_STR (400)
 
@@ -12,18 +13,18 @@ typedef struct ELLIPTICA_ID_READER_T
   const char *ifields;// input fields, e.g., "alpha,betax,adm_gxx"
   char *system;// the ID system, e.g., BHNS, NSNS, etc.
   char *option;// options for exportation, e.g., asymptotically_inertial
-  Uint nfield;// total number of Field_Dictionary
+  unsigned nfield;// total number of Field_Dictionary
   double **field;// value = field[indx(Field_Dictionary[i])][ijk]
   double npoints;// number of (x,y,z) coords
   double *x_coords;// Cartesian x coord values, x_coords[ijk]
   double *y_coords;// Cartesian y coord values, y_coords[ijk]
   double *z_coords;// Cartesian z coord values, z_coords[ijk]
-  Uint (*indx)(const char *const fname);// find field index  
+  unsigned (*indx)(const char *const fname);// find field index  
   void (*param)(const char *const lv,const char *const rv,
                 struct ELLIPTICA_ID_READER_T *const idr);// set params from evo code
   char **params_lv;// left value,  params[0]="force_balance"
   char **params_rv;// right value, params[0]="on"
-  Uint nparams;// number of params
+  unsigned nparams;// number of params
 }Elliptica_ID_Reader_T;
 
 Elliptica_ID_Reader_T *elliptica_id_reader_init (
