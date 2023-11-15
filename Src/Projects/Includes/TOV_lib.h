70305e113ce46eeceb978ae7bc99c63df99d2f8c
Alireza <rashti.alireza@gmail.com>
2023-11-15 16:23:18 -0500

    Revert TOV_lib.h changes to previous version.

--- Src/Projects/Includes/TOV_lib.h
+++ Src/Projects/Includes/TOV_lib.h
@@ -14,16 +14,12 @@ typedef struct TOV_PROJECT_T
  double h_cent;/* enthalpy at the center of NS */
  Uint N;/* number of interpolation points, choose them to be odd */
  double *m;/* total mass at each point */
- double *m0;/* Rest-mass at each point */
  double *r;/* radius at each point for metric inside the star:
            // ds^{2}=-e^{2\phi \left( r\right) }dt^{2}+\left( 1-\frac {2m\left( r\right) }{r}\right) ^{-1}dr^{2}+r^{2}\left( d\theta^{2}+sin^{2} \left(\theta \right)d\phi^{2}) */
  double *p;/* pressure at each point */
- double *e;/* total energy density at each point */
- double *rho0;/* rest-mass density at each point */
  double *h;/* enthalpy at each point */
- double *eps;/* Specific internal energy at each point */
  double *phi;/* at the spacetime metric g_00 = - exp[2phi] */
- double *rbar;/* rbar -> radius at each point for metric inside the star:
+ double *rbar; /* rbar -> radius at each point for metric inside the star:
              // d^{2}_{s}=-e^{2\phi }dt^{2}+\psi^{4}\left( \overline {r}\right) \left( d\overline {r}^{2}+\overline {r}^{2}d\Omega \right) */
  double *psi;/* conformal factor */
  Uint status:1;/* status of exist: 0 = no error happens, 1 = some errors happened. */
