16f00705d5cf556f85482e4ad816c06018888c61
Alireza <rashti.alireza@gmail.com>
2024-03-19 16:59:11 -0400

    Adapt stand alone reader script.

--- Scripts/Misc/Reader_Lib/clone_id_reader.sh
+++ Scripts/Misc/Reader_Lib/clone_id_reader.sh
@@ -169,6 +169,9 @@ int BH_NS_Binary_Initial_Data(void *vp)
   /* if this is a generic ID reader call */
   if (strcmp_i(PgetsEZ("IDR_BHNS_export_id"),"generic"))
     bhns_export_id_generic(vp);
+  /* if this is a generic ID reader call and MT safe */
+  else if (strcmp_i(PgetsEZ("IDR_BHNS_export_id"),"generic_MT_safe"))
+    bhns_export_id_generic_mt_safe(vp);
   else
     Error1(NO_OPTION);
 
